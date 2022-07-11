37b469e23b2307eec741eb7ac95a23de3afa5d84
Arkadiusz <arkadiusz.pasciak@ffwagency.com>
2022-07-11 11:55:04 +0200

    W98-0005: add component UITextarea

---
+++ src/Bundles/UIBundle/Tests/unit/UITextarea.unit.ts
@@ -0,0 +1,40 @@
+import { mount } from '@vue/test-utils'
+import { expect, it, describe } from 'vitest'
+import UITextarea from '@Bundles/UIBundle/Components/UITextarea.vue'
+
+const defaultWrapper = mount(UITextarea, {
+  props: {
+    variant: 'primary',
+    id: 'comment',
+    modelValue: 'test',
+    labelName: 'Email',
+    labelPosition: 'left',
+  },
+})
+
+const textarea = defaultWrapper.find('.textarea')
+const label = defaultWrapper.find('.label')
+
+describe('[UIBundle]<Components>(UITextarea)', async () => {
+  it('should have classes: main, variant and label position class', () => {
+    expect(defaultWrapper.attributes('class')).toBe(
+      'UITextarea v-primary l-p-left'
+    )
+  })
+
+  it('should have html structure', () => {
+    expect(defaultWrapper.html()).toBe(
+      '<div class="UITextarea v-primary l-p-left" data-v-437e0bb6=""><label class="label" for="comment" data-v-437e0bb6="">Email</label><textarea id="comment" class="textarea" data-v-437e0bb6=""></textarea></div>'
+    )
+  })
+
+  it('should have attributes: id, for', () => {
+    expect(textarea.attributes('id')).toBe('comment')
+    expect(label.attributes('for')).toBe('comment')
+  })
+
+  it('should be visible: textarea and label', () => {
+    expect(textarea.isVisible()).toBe(true)
+    expect(label.isVisible()).toBe(true)
+  })
+})
