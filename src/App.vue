26066025b8f6d0e5b2e0ce8d0fdb220b112e969b
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2022-07-12 15:24:08 +0200

    W98-0011: add bundle Pulpit
    Co-authored-by: Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,4 @@
   import HomePage from '@Views/HomePage/HomePage.vue'
 </script>
 
-<style lang="scss">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    margin-top: 60px;
-    color: $cl-white;
-  }
-</style>
+<style lang="scss"></style>
