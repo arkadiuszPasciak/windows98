0d76dc4a8a8361a6deadcc3257759c912f5a6db3
Arkadiusz <arkadiusz.pasciak@ffwagency.com>
2022-07-13 08:15:27 +0200

    W98-0012: add bundle Navigation

---
+++ src/Bundles/NavigationBundle/Components/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="Navigation"></nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  @import '@Bundles/NavigationBundle/Styles/Navigation.styles.scss';
+</style>
--- src/Views/HomePage/HomePage.vue
+++ src/Views/HomePage/HomePage.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="HomePage">
     <Pulpit />
+    <Navigation />
   </div>
 </template>
 
 <script setup lang="ts">
+  import Navigation from '@Bundles/NavigationBundle/Components/Navigation.vue'
   import Pulpit from '@Bundles/PulpitBundle/Components/Pulpit.vue'
 </script>
 
