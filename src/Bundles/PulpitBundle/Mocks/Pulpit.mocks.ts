cb524d0eb7f59afac38dc300c636c4869f0bc8cd
Arkadiusz <arkadiusz.pasciak@ffwagency.com>
2022-09-04 15:18:48 +0200

    W98-0014: move to mocks

---
+++ src/Bundles/PulpitBundle/Mocks/Pulpit.mocks.ts
@@ -0,0 +1,12 @@
+import { IPulpitStoreApps } from '@Bundles/PulpitBundle/Support/Pulpit.support'
+
+export const pulpitStoreApps = [
+  {
+    name: 'My Computer',
+    iconName: 'mycomputer',
+  },
+  {
+    name: 'Recycle Bin',
+    iconName: 'recycle',
+  },
+] as IPulpitStoreApps[]
