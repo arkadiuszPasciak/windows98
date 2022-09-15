fd999bb19953282bb3c5ce13e25e4d0f3d74c576
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2022-09-15 11:53:58 +0200

    W98-0015: refactor StartBundle

---
+++ src/Bundles/StartBundle/Mocks/Start.mocks.ts
@@ -0,0 +1,74 @@
+import { IStartStoreApps } from '@Bundles/StartBundle/Support/Start.support'
+
+export const startStoreApps = [
+  {
+    data: {
+      name: 'StartBundle.programs',
+      iconName: 'programs',
+    },
+    items: [
+      {
+        data: {
+          name: 'StartBundle.accessories',
+          iconName: 'programs',
+        },
+        items: [
+          {
+            data: {
+              name: 'StartBundle.calculator',
+              iconName: 'calculator',
+            },
+          },
+        ],
+      },
+      {
+        data: {
+          name: 'Microsoft Exchange',
+          iconName: 'microsoft-exchange',
+        },
+      },
+      {
+        data: {
+          name: 'MS-DOS Prompt',
+          iconName: 'ms-dos-prompt',
+        },
+      },
+    ],
+  },
+  {
+    data: {
+      name: 'StartBundle.documents',
+      iconName: 'documents',
+    },
+  },
+  {
+    data: {
+      name: 'StartBundle.settings',
+      iconName: 'settings',
+    },
+  },
+  {
+    data: {
+      name: 'StartBundle.find',
+      iconName: 'find',
+    },
+  },
+  {
+    data: {
+      name: 'StartBundle.help',
+      iconName: 'help',
+    },
+  },
+  {
+    data: {
+      name: 'StartBundle.run',
+      iconName: 'run',
+    },
+  },
+  {
+    data: {
+      name: 'StartBundle.shut-down',
+      iconName: 'shut-down',
+    },
+  },
+] as IStartStoreApps[]
