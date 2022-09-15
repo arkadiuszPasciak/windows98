49eba8ad7f5ce13a4df59c62c6d613de01636dd3
Arkadiusz <arkadiusz.pasciak@ffwagency.com>
2022-09-15 14:45:22 +0200

    W98-0022: update styles file names

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { i18n } from '@Configs/I18n/I18n.config'
 import App from '@/App.vue'
-import '@Assets/Styles/General/base.scss'
+import '@Assets/Styles/General/Base.styles.scss'
 
 const app = createApp(App)
 const pinia = createPinia()
