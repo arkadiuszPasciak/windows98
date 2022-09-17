4b5f2c6f18111cb90dd084cb8583a3387af256f7
Arkadiusz <arkadiusz.pasciak@ffwagency.com>
2022-09-17 16:43:26 +0200

    W98-0026: add unit tests useClockStore

--- src/Bundles/ClockBundle/Tests/units/Clock.units.ts
+++ src/Bundles/ClockBundle/Tests/units/Clock.units.ts
@@ -1,5 +1,7 @@
 import { expect, it, describe } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
 import { getTimeClock } from '@Bundles/ClockBundle/Services/Clock.services'
+import { useClockStore } from '@Bundles/ClockBundle/Stores/Clock.stores'
 
 describe('[ClockBundle]<Services>(getTimeClock)', () => {
   it('should return short time', () => {
@@ -22,5 +24,22 @@ describe('[ClockBundle]<Services>(getTimeClock)', () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     expect(getTimeClock()).toBe(null)
+  })
+})
+
+describe('[ClockBundle]<Stores>(useClockStore)', () => {
+  setActivePinia(createPinia())
+  const clock = useClockStore()
+
+  it('has default time', () => {
+    expect(clock.time).toBe('12:00 PM')
+  })
+
+  it('changes time on updateTime', () => {
+    clock.time = 'default value to check update'
+    const defaultValue = clock.time
+
+    clock.updateTime()
+    expect(clock.time).length(5).not.equal(defaultValue)
   })
 })
