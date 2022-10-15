9eae98266a85bcddcdf1ef4c695ff282a7b7a104
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-10-15 10:50:17 +0200

    add template UIButton

---
+++ src/Bundles/UIBundle/Components/UIModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="UIModal" :class="`variant-${variant}`">
+    <div class="navigation">
+      <h3 class="title">{{ title }}</h3>
+      <UIButton class="button-close">
+        x
+        <!-- <template #icon-left>
+          <img
+            class="button-icon"
+            :width="23"
+            :height="23"
+            src="src of image"
+            alt="close window"
+          />
+        </template> -->
+      </UIButton>
+    </div>
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { PropType } from 'vue'
+  import UIButton from '@Bundles/UIBundle/Components/UIButton.vue'
+
+  defineProps({
+    variant: {
+      type: String as PropType<'primary'>,
+      default: 'primary',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  })
+</script>
+
+<style lang="scss" scoped>
+  @import '@Bundles/UIBundle/Styles/UIModal.styles.scss';
+</style>
