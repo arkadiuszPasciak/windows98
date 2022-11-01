8dc83419587da60f12c3fa5349dd38981ce32293
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-01 17:22:30 +0100

    add template in CalculatorBundle

---
+++ src/Bundles/CalculatorBundle/Components/Calculator.vue
@@ -0,0 +1,44 @@
+<template>
+  <UIModal
+    class="Calculator"
+    :title="t('Calculator.title')"
+    :resize-window="false"
+    :width="195"
+    :height="235"
+    :modal-state="true"
+  >
+    <div class="content">
+      <div class="display"></div>
+      <UIButton>7</UIButton>
+      <UIButton>8</UIButton>
+      <UIButton>9</UIButton>
+      <UIButton>/</UIButton>
+      <UIButton>4</UIButton>
+      <UIButton>5</UIButton>
+      <UIButton>6</UIButton>
+      <UIButton>*</UIButton>
+      <UIButton>1</UIButton>
+      <UIButton>2</UIButton>
+      <UIButton>3</UIButton>
+      <UIButton>-</UIButton>
+      <UIButton>0</UIButton>
+      <UIButton>+/-</UIButton>
+      <UIButton>.</UIButton>
+      <UIButton>+</UIButton>
+    </div>
+  </UIModal>
+</template>
+
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n'
+  import UIButton from '@Bundles/UIBundle/Components/UIButton.vue'
+  import UIModal from '@Bundles/UIBundle/Components/UIModal.vue'
+
+  const { t } = useI18n()
+</script>
+
+<i18n src="@Bundles/ShutDownBundle/Locales/ShutDown.locales.json"></i18n>
+
+<style lang="scss" scoped>
+  @import '@Bundles/ShutDownBundle/Styles/ShutDown.styles.scss';
+</style>
