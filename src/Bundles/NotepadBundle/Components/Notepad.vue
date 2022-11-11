25cf2ec81628baca981ceb322e4a6c71ae78cc24
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-11 12:00:50 +0100

    style notepad

--- src/Bundles/NotepadBundle/Components/Notepad.vue
+++ src/Bundles/NotepadBundle/Components/Notepad.vue
@@ -8,22 +8,34 @@
     :modal-state="programStore.modalNotepad"
     @close-modal="closeModal"
   >
-    <div class="content">content will soon</div>
+    <div class="content">
+      <UITextarea
+        id="notepad-textarea"
+        type="notepad"
+        class="textarea"
+        :model-value="modelValue"
+        label-name=""
+        :resize-window="true"
+      />
+    </div>
   </UIModal>
 </template>
 
 <script setup lang="ts">
+  import { ref, Ref } from 'vue'
   import { useI18n } from 'vue-i18n'
   import UIModal from '@Bundles/UIBundle/Components/UIModal.vue'
+  import UITextarea from '@Bundles/UIBundle/Components/UITextarea.vue'
   import { useProgramStore } from '@Bundles/ProgramBundle/Stores/Program.stores'
 
+  const { t } = useI18n()
   const programStore = useProgramStore()
+
+  const modelValue = ref('This is an example!') as Ref<string>
 
   const closeModal = (): void => {
     programStore.updateNotepadModal(false)
   }
-
-  const { t } = useI18n()
 </script>
 
 <i18n src="@Bundles/NotepadBundle/Locales/Notepad.locales.json"></i18n>
