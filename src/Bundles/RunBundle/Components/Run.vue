c1ef00b945c2a2ec0ff1b5f082b6032dbf28f906
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-11 14:05:24 +0100

    add icon run

--- src/Bundles/RunBundle/Components/Run.vue
+++ src/Bundles/RunBundle/Components/Run.vue
@@ -13,7 +13,7 @@
         class="icon"
         width="32"
         height="32"
-        src="src/Assets/Icons/Applications/mycomputer.png"
+        src="src/Assets/Icons/Applications/rung.png"
         alt="Run computer"
       />
       <UIText class="description">{{ t('RunBundle.description') }}</UIText>
