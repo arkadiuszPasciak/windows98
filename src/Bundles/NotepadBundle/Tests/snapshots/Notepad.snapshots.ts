fe623c330b8eb4c1d70e0d2f0ced69cc1c4308b9
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-12 14:07:31 +0100

    fix problem with stories in spanshots

--- src/Bundles/NotepadBundle/Tests/snapshots/Notepad.snapshots.ts
+++ src/Bundles/NotepadBundle/Tests/snapshots/Notepad.snapshots.ts
@@ -1,8 +1,19 @@
 import { mount } from '@vue/test-utils'
 import { expect, it, describe } from 'vitest'
+import { createTestingPinia } from '@pinia/testing'
 import Notepad from '@Bundles/NotepadBundle/Components/Notepad.vue'
 
-const defaultWrapper = mount(Notepad)
+const defaultWrapper = mount(Notepad, {
+  global: {
+    plugins: [
+      createTestingPinia({
+        initialState: {
+          program: { modalNotepad: true },
+        },
+      }),
+    ],
+  },
+})
 
 describe('[NotepadBundle]<Snapshots>(Notepad)', async () => {
   it('renders correctly component', () => {
--- src/Bundles/RunBundle/Tests/snapshots/Run.snapshots.ts
+++ src/Bundles/RunBundle/Tests/snapshots/Run.snapshots.ts
@@ -1,8 +1,19 @@
 import { mount } from '@vue/test-utils'
 import { expect, it, describe } from 'vitest'
+import { createTestingPinia } from '@pinia/testing'
 import Run from '@Bundles/RunBundle/Components/Run.vue'
 
-const defaultWrapper = mount(Run)
+const defaultWrapper = mount(Run, {
+  global: {
+    plugins: [
+      createTestingPinia({
+        initialState: {
+          program: { modalRun: true },
+        },
+      }),
+    ],
+  },
+})
 
 describe('[RunBundle]<Snapshots>(Run)', async () => {
   it('renders correctly component', () => {
