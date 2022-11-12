a94eb4b4f2283ccf78252250433aa1087d1e6281
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-12 14:22:08 +0100

    init pinia per test

--- src/Bundles/AppBundle/Tests/units/App.units.ts
+++ src/Bundles/AppBundle/Tests/units/App.units.ts
@@ -1,8 +1,13 @@
 import { mount } from '@vue/test-utils'
 import { expect, it, describe } from 'vitest'
+import { createTestingPinia } from '@pinia/testing'
 import App from '@Bundles/AppBundle/Components/App.vue'
 
-const defaultWrapper = mount(App)
+const defaultWrapper = mount(App, {
+  global: {
+    plugins: [createTestingPinia()],
+  },
+})
 
 describe('[AppBundle]<Components>(App)', () => {
   it('mounted App', () => {
--- src/Configs/VueTestUtils.config.ts
+++ src/Configs/VueTestUtils.config.ts
@@ -1,10 +1,8 @@
 import { config } from '@vue/test-utils'
-import { createTestingPinia } from '@pinia/testing'
 import { i18n } from '@Configs/I18n.config'
 
 const plugins = {
-  pinia: createTestingPinia(),
   intlify: i18n,
 }
 
-config.global.plugins = [plugins.pinia, plugins.intlify]
+config.global.plugins = [plugins.intlify]
