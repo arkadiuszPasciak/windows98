7bef9c537fecb4c57928fb91fa450ac749c147d2
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-13 18:23:06 +0100

    add component, styles and locales for MediaPlayerBundle

---
+++ src/Bundles/MediaPlayerBundle/Components/ShutDown.vue
@@ -0,0 +1,33 @@
+<template>
+  <UIModal
+    class="MediaPlayer"
+    :title="t('MediaPlayer.title')"
+    :resize-window="false"
+    :width="330"
+    :height="180"
+    :modal-state="programStore.modalShutDown"
+    @close-modal="closeModal"
+  >
+    <div class="content">here will be content</div>
+  </UIModal>
+</template>
+
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n'
+  import UIModal from '@Bundles/UIBundle/Components/UIModal.vue'
+  import { useProgramStore } from '@Bundles/ProgramBundle/Stores/Program.stores'
+
+  const { t } = useI18n()
+
+  const programStore = useProgramStore()
+
+  const closeModal = (): void => {
+    programStore.updateShutDownModal(false)
+  }
+</script>
+
+<i18n src="@Bundles/MediaPlayerBundle/Locales/MediaPlayer.locales.json"></i18n>
+
+<style lang="scss" scoped>
+  @import '@Bundles/MediaPlayerBundle/Styles/MediaPlayer.styles.scss';
+</style>
