720cee590304a1a497be74c6f67a49c715077371
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-14 18:05:17 +0100

    add component MediaPlayerProgress and MediaPlayerControlButton

---
+++ src/Bundles/MediaPlayerBundle/Components/MediaPlayerControlButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    class="MediaPlayerControlButton"
+    :class="`type-${type}`"
+    type="button"
+    :aria-label="t(`MediaPlayer.control-button.${type}`)"
+  />
+</template>
+
+<script setup lang="ts">
+  import { PropType } from 'vue'
+  import { useI18n } from 'vue-i18n'
+
+  defineProps({
+    type: {
+      type: String as PropType<'play' | 'pauza' | 'stop'>,
+      required: true,
+    },
+  })
+
+  const { t } = useI18n()
+</script>
+
+<i18n src="@Bundles/MediaPlayerBundle/Locales/MediaPlayer.locales.json"></i18n>
+
+<style lang="scss" scoped>
+  @import '@Bundles/MediaPlayerBundle/Styles/MediaPlayerControlButton.styles.scss';
+</style>
---
+++ src/Bundles/MediaPlayerBundle/Components/MediaPlayerProgress.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="MediaPlayerProgress">
+    <progress id="progress-bar" value="0" min="0"></progress>
+    <input id="seek" class="seek" value="0" min="0" type="range" step="1" />
+    <div id="seek-tooltip" class="seek-tooltip">00:00</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  @import '@Bundles/MediaPlayerBundle/Styles/MediaPlayerProgress.styles.scss';
+</style>
