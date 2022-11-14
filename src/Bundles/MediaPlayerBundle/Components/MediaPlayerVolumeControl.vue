8b8869d9e5acf6334ba83e4320b9ae45e50034d6
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-14 18:16:13 +0100

    add component MediaPlayerVolumeControl

---
+++ src/Bundles/MediaPlayerBundle/Components/MediaPlayerVolumeControl.vue
@@ -0,0 +1,18 @@
+<template>
+  <input
+    id="volume"
+    class="MediaPlayerVolumeControl"
+    value="1"
+    data-mute="0.5"
+    type="range"
+    max="1"
+    min="0"
+    step="0.01"
+  />
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  @import '@Bundles/MediaPlayerBundle/Styles/MediaPlayerVolumeControl.styles.scss';
+</style>
