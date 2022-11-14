56205284cd5443684775c2f49120d3588b743cc1
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-14 18:31:05 +0100

    add component MediaPlayerControls and MediaPlayerInformation

---
+++ src/Bundles/MediaPlayerBundle/Components/MediaPlayerControls.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="MediaPlayerControls"><slot /></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  @import '@Bundles/MediaPlayerBundle/Styles/MediaPlayerControls.styles.scss';
+</style>
---
+++ src/Bundles/MediaPlayerBundle/Components/MediaPlayerInformation.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="MediaPlayerInformation"><slot /></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  @import '@Bundles/MediaPlayerBundle/Styles/MediaPlayerInformation.styles.scss';
+</style>
