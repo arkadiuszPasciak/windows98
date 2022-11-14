39ea43271e57bde46b5c2e809f5d880cdaa4106b
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-14 17:42:40 +0100

    add component MediaPlayerVideo

--- src/Bundles/MediaPlayerBundle/Components/MediaPlayer.vue
+++ src/Bundles/MediaPlayerBundle/Components/MediaPlayer.vue
@@ -8,13 +8,16 @@
     :modal-state="programStore.modalMediaPlayer"
     @close-modal="closeModal"
   >
-    <div class="content">here will be content</div>
+    <div class="content">
+      <MediaPlayerVideo src="https://youtu.be/IW7Rqwwth84" />
+    </div>
   </UIModal>
 </template>
 
 <script setup lang="ts">
   import { useI18n } from 'vue-i18n'
   import UIModal from '@Bundles/UIBundle/Components/UIModal.vue'
+  import MediaPlayerVideo from '@Bundles/MediaPlayerBundle/Components/MediaPlayerVideo.vue'
   import { useProgramStore } from '@Bundles/ProgramBundle/Stores/Program.stores'
 
   const { t } = useI18n()
