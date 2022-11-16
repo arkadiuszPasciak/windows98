80ad71fc33137cb1070f440f79f46286be00b3d6
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-16 18:24:36 +0100

    style media player controls

--- src/Bundles/MediaPlayerBundle/Components/MediaPlayerControls.vue
+++ src/Bundles/MediaPlayerBundle/Components/MediaPlayerControls.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="MediaPlayerControls">
-    <MediaPlayerProgress />
-    <MediaPlayerButtonControl type="play" />
-    <MediaPlayerButtonControl type="pauza" />
-    <MediaPlayerButtonControl type="stop" />
-    <MediaPlayerButtonControl type="mute" />
-    <MediaPlayerVolumeControl />
+    <MediaPlayerProgress class="progress" />
+    <MediaPlayerButtonControl class="button-play" type="play" />
+    <MediaPlayerButtonControl class="button-pauza" type="pauza" />
+    <MediaPlayerButtonControl class="button-stop" type="stop" />
+    <MediaPlayerButtonControl class="button-mute" type="mute" />
+    <MediaPlayerVolumeControl class="volume-control" />
   </div>
 </template>
 
 <script setup lang="ts">
   import MediaPlayerButtonControl from '@Bundles/MediaPlayerBundle/Components/MediaPlayerButtonControl.vue'
   import MediaPlayerVolumeControl from '@Bundles/MediaPlayerBundle/Components/MediaPlayerVolumeControl.vue'
+  import MediaPlayerProgress from '@Bundles/MediaPlayerBundle/Components/MediaPlayerProgress.vue'
 </script>
 
 <style lang="scss" scoped>
