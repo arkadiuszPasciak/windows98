a0b1f45ea5436e87df1b88c974f30f6dcf74d237
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-11-17 18:02:58 +0100

    add styles media player progress

--- src/Bundles/MediaPlayerBundle/Components/MediaPlayerProgress.vue
+++ src/Bundles/MediaPlayerBundle/Components/MediaPlayerProgress.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="MediaPlayerProgress">
-    <progress id="progress-bar" value="0" min="0"></progress>
-    <input id="seek" class="seek" value="0" min="0" type="range" step="1" />
-    <div id="seek-tooltip" class="seek-tooltip">00:00</div>
+    <progress id="progress-bar" class="progress" value="0" min="0"></progress>
+    <input id="seek" class="input" value="0" min="0" type="range" step="1" />
   </div>
 </template>
 
