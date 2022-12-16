1058fc03d35a1479d1ab0c23c2e59f16d7f60c5d
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-12-16 17:30:40 +0100

    add bundle Settings

---
+++ src/Bundles/SettingsBundle/Components/Settings.vue
@@ -0,0 +1,34 @@
+<template>
+  <UIModal
+    class="Settings"
+    :title="t('SettingsBundle.title')"
+    :resize-window="false"
+    :width="335"
+    :height="415"
+    :modal-state="programStore.modalRun"
+    @close-modal="closeModal"
+  >
+    <div class="content">
+      <!-- ... here will be content -->
+    </div>
+  </UIModal>
+</template>
+
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n'
+  import UIModal from '@Bundles/UIBundle/Components/UIModal.vue'
+  import { useProgramStore } from '@Bundles/ProgramBundle/Stores/Program.stores'
+
+  const { t } = useI18n()
+  const programStore = useProgramStore()
+
+  const closeModal = (): void => {
+    programStore.updateRunModal(false)
+  }
+</script>
+
+<i18n src="@Bundles/SettingsBundle/Locales/Settings.locales.json"></i18n>
+
+<style lang="scss" scoped>
+  @import '@Bundles/SettingsBundle/Styles/Settings.styles.scss';
+</style>
---
+++ src/Bundles/SettingsBundle/Tests/snapshots/Settings.snapshots.ts
@@ -0,0 +1,22 @@
+import { mount } from '@vue/test-utils'
+import { expect, it, describe } from 'vitest'
+import { createTestingPinia } from '@pinia/testing'
+import Settings from '@Bundles/SettingsBundle/Components/Settings.vue'
+
+const defaultWrapper = mount(Settings, {
+  global: {
+    plugins: [
+      createTestingPinia({
+        initialState: {
+          program: { modalRun: true },
+        },
+      }),
+    ],
+  },
+})
+
+describe('[SettingsBundle]<Snapshots>(Settings)', async () => {
+  it('renders correctly component', () => {
+    expect(defaultWrapper.element).toMatchSnapshot()
+  })
+})
