fbab7f1205a48caa05b4dd1e317e0517e3360fcd
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-12-19 17:57:10 +0100

    fix name problem

--- src/Bundles/UIBundle/Components/UISelect.vue
+++ src/Bundles/UIBundle/Components/UISelect.vue
@@ -14,13 +14,14 @@
       :value="option.value"
       class="option"
     >
-      {{ option.name }}
+      {{ t(option.name) }}
     </option>
   </select>
 </template>
 
 <script setup lang="ts">
   import { computed, PropType } from 'vue'
+  import { useI18n } from 'vue-i18n'
   import { TUISelectPropsObject } from '@Bundles/UIBundle/Supports/UISelect.supports'
 
   const props = defineProps({
@@ -50,6 +51,8 @@
     },
   })
 
+  const { t } = useI18n()
+
   const emit = defineEmits(['update:modelValue'])
 
   const value = computed({
@@ -61,6 +64,8 @@
     },
   })
 </script>
+
+<i18n src="@Bundles/LanguageBundle/Locales/Language.locales.json"></i18n>
 
 <style lang="scss" scoped>
   @import '@Bundles/UIBundle/Styles/UISelect.styles.scss';
