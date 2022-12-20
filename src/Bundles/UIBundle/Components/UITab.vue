2d2e0646d274b1d68fcc39ae9cfb0bf281a53dec
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2022-12-20 17:07:10 +0100

    add template and styles for UITab

---
+++ src/Bundles/UIBundle/Components/UITab.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="UITab">
+    <div class="tabs">
+      <button class="tab" type="button">General</button>
+    </div>
+    <div class="contents">
+      <div class="content">here will be content</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  @import '@Bundles/UIBundle/Styles/UITab.styles.scss';
+</style>
