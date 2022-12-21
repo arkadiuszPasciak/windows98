0cd463552c40e62369943a2f8c62ced394aa5aa2
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2022-12-21 14:19:50 +0100

    Patch 0.1.7 UITabs (#28)

---
+++ src/Bundles/UIBundle/Supports/UITabs.supports.ts
@@ -0,0 +1,8 @@
+import { Component } from 'vue'
+
+interface IUITabProps {
+  title: string
+  component: Component
+}
+
+export type TUITabsProps = Array<IUITabProps>
---
+++ src/Bundles/UIBundle/Tests/snapshots/UITabs.snapshots.ts
@@ -0,0 +1,28 @@
+import { shallowMount } from '@vue/test-utils'
+import { expect, it, describe } from 'vitest'
+import UITabs from '@Bundles/UIBundle/Components/UITabs.vue'
+import UIText from '@Bundles/UIBundle/Components/UIText.vue'
+import { TUITabsProps } from '@Bundles/UIBundle/Supports/UITabs.supports'
+
+const tabs = [
+  {
+    title: 'Settings.general',
+    component: UIText,
+  },
+  {
+    title: 'Settings.general',
+    component: UIText,
+  },
+] as TUITabsProps
+
+const defaultWrapper = shallowMount(UITabs, {
+  props: {
+    tabs: tabs,
+  },
+})
+
+describe('[UIBundle]<Snapshots>(UITabs)', async () => {
+  it('renders correctly component', () => {
+    expect(defaultWrapper.element).toMatchSnapshot()
+  })
+})
