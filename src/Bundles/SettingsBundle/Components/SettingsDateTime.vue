65c4664563b189fa47f1bad9aa032bc49e749d14
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-01-09 16:07:10 +0100

    add new tab in settings

---
+++ src/Bundles/SettingsBundle/Components/SettingsDateTime.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="SettingsDateTime">
+    <MonthSwitcher />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import MonthSwitcher from '@Bundles/DateBundle/Components/MonthSwitcher.vue'
+</script>
+
+<style lang="scss" scoped>
+  @import '@Bundles/SettingsBundle/Styles/SettingsDateTime.styles.scss';
+</style>
