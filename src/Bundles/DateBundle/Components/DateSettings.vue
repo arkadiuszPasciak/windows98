a70f085b7eded9cb53520965b14d2dd2e4f0cc9d
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-01-11 09:21:31 +0100

    add component UIFrame and set in DateSettings

--- src/Bundles/DateBundle/Components/DateSettings.vue
+++ src/Bundles/DateBundle/Components/DateSettings.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="DateSettings">
+  <UIFrame class="DateSettings" :title="t('Date.title-date')">
     <DateMonthSwitcher />
     <DateYearSwitcher />
     <DateCalendar />
-  </div>
+  </UIFrame>
 </template>
 
 <script setup lang="ts">
+  import { useI18n } from 'vue-i18n'
   import DateMonthSwitcher from '@Bundles/DateBundle/Components/DateMonthSwitcher.vue'
   import DateYearSwitcher from '@Bundles/DateBundle/Components/DateYearSwitcher.vue'
   import DateCalendar from '@Bundles/DateBundle/Components/DateCalendar.vue'
+  import UIFrame from '@Bundles/UIBundle/Components/UIFrame.vue'
+
+  const { t } = useI18n()
 </script>
+
+<i18n src="@Bundles/DateBundle/Locales/Date.locales.json"></i18n>
 
 <style lang="scss" scoped>
   @import '@Bundles/DateBundle/Styles/DateSettings.styles.scss';
