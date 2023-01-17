87ec6a94977665080caa71f5044761b84a749c7a
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2023-01-17 13:21:35 +0100

    Patch 0.2.2 TimezoneBundle (#33)

--- src/Bundles/SettingsBundle/Components/SettingsDateTime.vue
+++ src/Bundles/SettingsBundle/Components/SettingsDateTime.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="SettingsDateTime">
     <CalendarSettings />
+    <TimezoneSwitcher />
   </div>
 </template>
 
 <script setup lang="ts">
   import CalendarSettings from '@Bundles/CalendarBundle/Components/CalendarSettings.vue'
+  import TimezoneSwitcher from '@Bundles/TimezoneBundle/Components/TimezoneSwitcher.vue'
 </script>
 
 <style lang="scss" scoped>
---
+++ src/Bundles/TimezoneBundle/Components/TimezoneSwitcher.vue
@@ -0,0 +1,20 @@
+<template>
+  <UISelect
+    id="timezones"
+    class="TimezoneSwitcher"
+    :model-value="currentTimezone ?? timezones[6].value"
+    :options="timezones"
+  />
+</template>
+
+<script setup lang="ts">
+  import UISelect from '@Bundles/UIBundle/Components/UISelect.vue'
+  import { timezones } from '@Bundles/TimezoneBundle/Mocks/Timezone.mocks'
+  import { getTimezone } from '@Bundles/TimezoneBundle/Services/Timezone.services'
+
+  const currentTimezone = getTimezone(new Date())
+</script>
+
+<style lang="scss" scoped>
+  @import '@Bundles/TimezoneBundle/Styles/TimezoneSwitcher.styles.scss';
+</style>
