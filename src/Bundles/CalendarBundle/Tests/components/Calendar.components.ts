d15354e2243abd4f4124cf97377e043ab0126ac8
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-02-07 18:48:18 +0100

    add class CalendarComponentTesting

--- src/Bundles/CalendarBundle/Tests/components/Calendar.components.ts
+++ src/Bundles/CalendarBundle/Tests/components/Calendar.components.ts
@@ -1,55 +1,9 @@
 import { createTestingPinia } from '@pinia/testing'
 import CalendarSettings from '@Bundles/CalendarBundle/Components/CalendarSettings.vue'
-import {
-  ECalendarYearChangeMethod,
-  TCalendarYearChangeMethod,
-} from '@Bundles/CalendarBundle/Supports/Calendar.supports'
+import { CalendarComponentTesting } from '@Bundles/CalendarBundle/Services/Calendar.services'
+import { ECalendarYearChangeMethod } from '@Bundles/CalendarBundle/Supports/Calendar.supports'
 
-const elements = {
-  calendarDaysView: '[data-test="calendar-view-days"]',
-  selectDateMonths: '[data-test="ui-select-date-months"]',
-  optionSelected: '[data-test="ui-select-date-months"] > option:selected',
-  buttonYear: (method: TCalendarYearChangeMethod) =>
-    `[data-test="calendar-year-switcher-${method}"]`,
-  showYear: '[data-test="calendar-year-switcher-year"]',
-}
-
-function changeSelectAndCheckComponent(
-  month: string,
-  inActiveDays: number,
-  days: number,
-) {
-  cy.get(elements.selectDateMonths).select(month)
-
-  cy.get(elements.optionSelected).should('have.text', month)
-
-  cy.get(elements.calendarDaysView)
-    .find('.day.is-inactive')
-    .should('have.length', inActiveDays)
-
-  cy.get(elements.calendarDaysView)
-    .find('.day.is-normal')
-    .should('have.length', days)
-}
-
-function changeYearAndCheckComponent(
-  year: string,
-  method: TCalendarYearChangeMethod,
-  inActiveDays: number,
-  days: number,
-) {
-  cy.get(elements.buttonYear(method)).click()
-
-  cy.get(elements.showYear).should('have.text', year)
-
-  cy.get(elements.calendarDaysView)
-    .find('.day.is-inactive')
-    .should('have.length', inActiveDays)
-
-  cy.get(elements.calendarDaysView)
-    .find('.day.is-normal')
-    .should('have.length', days)
-}
+const methods = new CalendarComponentTesting()
 
 describe('[CalendarBundle]<Components>(Calendar)', async () => {
   beforeEach(() => {
@@ -76,47 +30,47 @@ describe('[CalendarBundle]<Components>(Calendar)', async () => {
   })
 
   it('selects months', () => {
-    changeSelectAndCheckComponent('January', 0, 31)
+    methods.changeSelectAndCheckComponent('January', 0, 31)
 
-    changeSelectAndCheckComponent('February', 3, 28)
+    methods.changeSelectAndCheckComponent('February', 3, 28)
 
-    changeSelectAndCheckComponent('March', 3, 31)
+    methods.changeSelectAndCheckComponent('March', 3, 31)
 
-    changeSelectAndCheckComponent('April', 6, 30)
+    methods.changeSelectAndCheckComponent('April', 6, 30)
 
-    changeSelectAndCheckComponent('May', 1, 31)
+    methods.changeSelectAndCheckComponent('May', 1, 31)
 
-    changeSelectAndCheckComponent('June', 4, 30)
+    methods.changeSelectAndCheckComponent('June', 4, 30)
 
-    changeSelectAndCheckComponent('July', 6, 31)
+    methods.changeSelectAndCheckComponent('July', 6, 31)
 
-    changeSelectAndCheckComponent('August', 2, 31)
+    methods.changeSelectAndCheckComponent('August', 2, 31)
 
-    changeSelectAndCheckComponent('September', 5, 30)
+    methods.changeSelectAndCheckComponent('September', 5, 30)
 
-    changeSelectAndCheckComponent('October', 0, 31)
+    methods.changeSelectAndCheckComponent('October', 0, 31)
 
-    changeSelectAndCheckComponent('November', 3, 30)
+    methods.changeSelectAndCheckComponent('November', 3, 30)
 
-    changeSelectAndCheckComponent('December', 5, 31)
+    methods.changeSelectAndCheckComponent('December', 5, 31)
   })
 
   it('selects years', () => {
-    changeYearAndCheckComponent(
+    methods.changeYearAndCheckComponent(
       '2024',
       ECalendarYearChangeMethod.INCREASE,
       4,
       29,
     )
 
-    changeYearAndCheckComponent(
+    methods.changeYearAndCheckComponent(
       '2023',
       ECalendarYearChangeMethod.DECREASE,
       3,
       28,
     )
 
-    changeYearAndCheckComponent(
+    methods.changeYearAndCheckComponent(
       '2022',
       ECalendarYearChangeMethod.DECREASE,
       2,
