8fbdcc9869a94f410076bc7c2706572312f61c70
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-02-11 14:29:57 +0100

    add component tests for PulpitBundle

---
+++ src/Bundles/PulpitBundle/Tests/components/PulpitItem.components.ts
@@ -0,0 +1,20 @@
+import PulpitItem from '@Bundles/PulpitBundle/Components/PulpitItem.vue'
+
+describe('[PulpitBundle]<Components>(PulpitItem)', async () => {
+  beforeEach(() => {
+    cy.viewport(75, 65)
+
+    cy.mount(PulpitItem, {
+      props: {
+        name: 'My Computer',
+        iconName: 'mycomputer',
+      },
+    })
+  })
+
+  it('should have element and class', () => {
+    cy.get('div.PulpitItem')
+
+    cy.contains('My Computer')
+  })
+})
