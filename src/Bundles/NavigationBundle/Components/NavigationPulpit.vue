a600e72a3176e4e107010bb8611d257d2e3d9ae9
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-02-17 08:53:07 +0100

    add component NavigationPulpit

---
+++ src/Bundles/NavigationBundle/Components/NavigationPulpit.vue
@@ -0,0 +1,16 @@
+<template>
+  <NavigationGrid class="NavigationPulpit">
+    <Start />
+    <AudioButton />
+    <Clock />
+  </NavigationGrid>
+</template>
+
+<script setup lang="ts">
+  import AudioButton from '@Bundles/AudioBundle/Components/AudioButton.vue'
+  import Clock from '@Bundles/ClockBundle/Components/Clock.vue'
+  import NavigationGrid from '@Bundles/NavigationBundle/Components/NavigationGrid.vue'
+  import Start from '@Bundles/StartBundle/Components/Start.vue'
+</script>
+
+<style lang="scss" scoped></style>
--- src/Views/HomePage/HomePage.vue
+++ src/Views/HomePage/HomePage.vue
@@ -4,20 +4,13 @@
 
     <Pulpit />
 
-    <Navigation>
-      <Start />
-      <AudioButton />
-      <Clock />
-    </Navigation>
+    <NavigationPulpit />
   </div>
 </template>
 
 <script setup lang="ts">
-  import AudioButton from '@Bundles/AudioBundle/Components/AudioButton.vue'
-  import Clock from '@Bundles/ClockBundle/Components/Clock.vue'
-  import Navigation from '@Bundles/NavigationBundle/Components/Navigation.vue'
+  import NavigationPulpit from '@Bundles/NavigationBundle/Components/NavigationPulpit.vue'
   import Pulpit from '@Bundles/PulpitBundle/Components/Pulpit.vue'
-  import Start from '@Bundles/StartBundle/Components/Start.vue'
   import TeleportPulpit from '@Bundles/TeleportBundle/Components/TeleportPulpit.vue'
 </script>
 
