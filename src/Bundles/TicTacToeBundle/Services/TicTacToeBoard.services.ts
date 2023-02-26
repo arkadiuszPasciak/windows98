8201a800c7252088515c0341aa1e8950e06eadf0
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-02-26 08:47:23 +0100

    add unit tests for TicTacToeBoard

--- src/Bundles/TicTacToeBundle/Services/TicTacToeBoard.services.ts
+++ src/Bundles/TicTacToeBundle/Services/TicTacToeBoard.services.ts
@@ -6,48 +6,19 @@ import {
 
 export class TicTacToeBoard {
   public dimension: TTicTacToeRadioDimension
-  public amountOfFields: number
+  public columnAndRow: Array<string>
   public amountOfColumns: number
+  public amountOfFields: number
   public amountOfRows: number
   public currentBoard: Array<string>
 
   constructor(dimension: TTicTacToeRadioDimension) {
     this.dimension = dimension
-    this.amountOfFields = this.getAmountOfFields()
+    this.columnAndRow = this.getColumnAndRow()
     this.amountOfColumns = this.getAmountOfColumns()
+    this.amountOfFields = this.getAmountOfFields()
     this.amountOfRows = this.getAmountOfRows()
     this.currentBoard = this.getFieldsOfBoard()
-  }
-
-  public getAmountOfFields(): number {
-    const columnAndRow = this.getColumnAndRow()
-
-    return Number(columnAndRow[0]) * Number(columnAndRow[1])
-  }
-
-  public getAmountOfColumns(): number {
-    const columnAndRow = this.getColumnAndRow()
-
-    return Number(columnAndRow[0])
-  }
-
-  public getAmountOfRows(): number {
-    const columnAndRow = this.getColumnAndRow()
-
-    return Number(columnAndRow[1])
-  }
-
-  public getFieldsOfBoard(): Array<string> {
-    const array = [] as Array<string>
-
-    let fieldIndex = 1 as number
-
-    do {
-      array.push('')
-      fieldIndex++
-    } while (fieldIndex <= this.amountOfFields)
-
-    return array
   }
 
   public setColorByTextContent(event: Event): void {
@@ -71,7 +42,32 @@ export class TicTacToeBoard {
     eventTarget.style.color = colorStyle
   }
 
+  private getAmountOfColumns(): number {
+    return Number(this.columnAndRow[0])
+  }
+
+  private getAmountOfFields(): number {
+    return Number(this.columnAndRow[0]) * Number(this.columnAndRow[1])
+  }
+
+  private getAmountOfRows(): number {
+    return Number(this.columnAndRow[1])
+  }
+
   private getColumnAndRow(): Array<string> {
     return this.dimension.split('x')
+  }
+
+  private getFieldsOfBoard(): Array<string> {
+    const array = [] as Array<string>
+
+    let fieldIndex = 1 as number
+
+    do {
+      array.push('')
+      fieldIndex++
+    } while (fieldIndex <= this.amountOfFields)
+
+    return array
   }
 }
