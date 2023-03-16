5fa70d8a13cdf28b3f62623c5267a104eca89efd
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-03-16 15:00:15 +0100

    add documentation App

--- src/Bundles/DocumentationBundle/Services/Documentation.services.ts
+++ src/Bundles/DocumentationBundle/Services/Documentation.services.ts
@@ -1,3 +1,9 @@
+import AppDocumentation from '@Bundles/AppBundle/Documentations/App.documentations.md'
 import UserAgentDocumentation from '@Bundles/UserAgentBundle/Documentations/UserAgent.documentations.md'
 
-export const testFirst = UserAgentDocumentation
+export const testFirst = AppDocumentation
+
+export const documentations = {
+  AppDocumentation,
+  UserAgentDocumentation,
+}
