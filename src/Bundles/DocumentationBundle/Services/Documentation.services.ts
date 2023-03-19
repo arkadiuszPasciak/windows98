f2013fb53accf3d9b0fd144c05ca6e34913dcfff
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-03-19 10:37:12 +0100

    add documentation CalculatorBundle

--- src/Bundles/DocumentationBundle/Services/Documentation.services.ts
+++ src/Bundles/DocumentationBundle/Services/Documentation.services.ts
@@ -1,11 +1,13 @@
 import AppDocumentation from '@Bundles/AppBundle/Documentations/App.documentations.md'
 import AudioDocumentation from '@Bundles/AudioBundle/Documentations/Audio.documentations.md'
+import CalculatorDocumentation from '@Bundles/CalculatorBundle/Documentations/Calculator.documentations.md'
 import UserAgentDocumentation from '@Bundles/UserAgentBundle/Documentations/UserAgent.documentations.md'
 
-export const testFirst = AudioDocumentation
+export const testFirst = CalculatorDocumentation
 
 export const documentations = {
   AppDocumentation,
   AudioDocumentation,
+  CalculatorDocumentation,
   UserAgentDocumentation,
 }
