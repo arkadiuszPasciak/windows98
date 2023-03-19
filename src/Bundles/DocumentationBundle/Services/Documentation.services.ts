5d5d36521eda8e0a138645987a60b60f9e036258
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-03-19 13:35:36 +0100

    add documentation CalendarBundle

--- src/Bundles/DocumentationBundle/Services/Documentation.services.ts
+++ src/Bundles/DocumentationBundle/Services/Documentation.services.ts
@@ -1,13 +1,15 @@
 import AppDocumentation from '@Bundles/AppBundle/Documentations/App.documentations.md'
 import AudioDocumentation from '@Bundles/AudioBundle/Documentations/Audio.documentations.md'
 import CalculatorDocumentation from '@Bundles/CalculatorBundle/Documentations/Calculator.documentations.md'
+import CalendarDocumentation from '@Bundles/CalendarBundle/Documentations/Calendar.documentations.md'
 import UserAgentDocumentation from '@Bundles/UserAgentBundle/Documentations/UserAgent.documentations.md'
 
-export const testFirst = CalculatorDocumentation
+export const testFirst = CalendarDocumentation
 
 export const documentations = {
   AppDocumentation,
   AudioDocumentation,
   CalculatorDocumentation,
+  CalendarDocumentation,
   UserAgentDocumentation,
 }
