75df59325a1e998f4d093fac9f35e38049c71e44
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-03-19 10:23:46 +0100

    add documentation AudioBundle

--- src/Bundles/DocumentationBundle/Services/Documentation.services.ts
+++ src/Bundles/DocumentationBundle/Services/Documentation.services.ts
@@ -1,9 +1,11 @@
 import AppDocumentation from '@Bundles/AppBundle/Documentations/App.documentations.md'
+import AudioDocumentation from '@Bundles/AudioBundle/Documentations/Audio.documentations.md'
 import UserAgentDocumentation from '@Bundles/UserAgentBundle/Documentations/UserAgent.documentations.md'
 
-export const testFirst = AppDocumentation
+export const testFirst = AudioDocumentation
 
 export const documentations = {
   AppDocumentation,
+  AudioDocumentation,
   UserAgentDocumentation,
 }
