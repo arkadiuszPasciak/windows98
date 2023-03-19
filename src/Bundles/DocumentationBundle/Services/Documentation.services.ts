eda17f72a9c351cc9572cd35a3c14f734aeb2dc6
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-03-19 13:44:17 +0100

    add documentation ClockBundle

--- src/Bundles/DocumentationBundle/Services/Documentation.services.ts
+++ src/Bundles/DocumentationBundle/Services/Documentation.services.ts
@@ -2,14 +2,16 @@ import AppDocumentation from '@Bundles/AppBundle/Documentations/App.documentatio
 import AudioDocumentation from '@Bundles/AudioBundle/Documentations/Audio.documentations.md'
 import CalculatorDocumentation from '@Bundles/CalculatorBundle/Documentations/Calculator.documentations.md'
 import CalendarDocumentation from '@Bundles/CalendarBundle/Documentations/Calendar.documentations.md'
+import ClockDocumentation from '@Bundles/ClockBundle/Documentations/Clock.documentations.md'
 import UserAgentDocumentation from '@Bundles/UserAgentBundle/Documentations/UserAgent.documentations.md'
 
-export const testFirst = CalendarDocumentation
+export const testFirst = ClockDocumentation
 
 export const documentations = {
   AppDocumentation,
   AudioDocumentation,
   CalculatorDocumentation,
   CalendarDocumentation,
+  ClockDocumentation,
   UserAgentDocumentation,
 }
