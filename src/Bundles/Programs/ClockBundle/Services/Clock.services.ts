5d2bd9a334e39bfd5af21b74a98362c098ec282a
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-04-02 12:59:21 +0200

    fix clock for american users and add manually trigger ci

--- src/Bundles/Programs/ClockBundle/Services/Clock.services.ts
+++ src/Bundles/Programs/ClockBundle/Services/Clock.services.ts
@@ -1,6 +1,18 @@
 import { Nullable } from 'vitest'
 import { IClockGetTime } from '@Bundles/ClockBundle/Supports/Clock.supports'
 
+export function isAmericanTime(time: string): Nullable<boolean> {
+  if (!time) {
+    return null
+  }
+
+  if (time.includes('PM') || time.includes('AM')) {
+    return true
+  } else {
+    return false
+  }
+}
+
 export function getTimeClock(params: IClockGetTime): Nullable<string> {
   if (!params) {
     return null
--- src/Bundles/Programs/ClockBundle/Tests/units/Clock.units.ts
+++ src/Bundles/Programs/ClockBundle/Tests/units/Clock.units.ts
@@ -1,23 +1,38 @@
 import { expect, it, describe } from 'vitest'
 import { setActivePinia, createPinia } from 'pinia'
-import { getTimeClock } from '@Bundles/ClockBundle/Services/Clock.services'
+import {
+  isAmericanTime,
+  getTimeClock,
+} from '@Bundles/ClockBundle/Services/Clock.services'
 import { useClockStore } from '@Bundles/ClockBundle/Stores/Clock.stores'
 
 describe('[ClockBundle]<Services>(getTimeClock)', () => {
   it('should return short time', () => {
-    expect(
-      getTimeClock({
-        timeStyle: 'short',
-      }),
-    ).length(5)
+    const time = getTimeClock({
+      timeStyle: 'short',
+    })
+
+    expect(time).not.toBe(null)
+
+    if (time && isAmericanTime(time)) {
+      expect(time).length(8)
+    } else if (time) {
+      expect(time).length(5)
+    }
   })
 
   it('should return medium time', () => {
-    expect(
-      getTimeClock({
-        timeStyle: 'medium',
-      }),
-    ).length(8)
+    const time = getTimeClock({
+      timeStyle: 'medium',
+    })
+
+    expect(time).not.toBe(null)
+
+    if (time && isAmericanTime(time)) {
+      expect(time).length(11)
+    } else if (time) {
+      expect(time).length(8)
+    }
   })
 
   it('should return null if params are falsy or empty', () => {
@@ -37,9 +52,16 @@ describe('[ClockBundle]<Stores>(useClockStore)', () => {
 
   it('changes time on updateTime', () => {
     clock.time = 'default value to check update'
-    const defaultValue = clock.time
+    const defaultValue = clock.time as string
 
     clock.updateTime()
-    expect(clock.time).length(5).not.equal(defaultValue)
+
+    expect(clock.time).not.toBe(null)
+
+    if (isAmericanTime(clock.time)) {
+      expect(clock.time).length(8).not.equal(defaultValue)
+    } else {
+      expect(clock.time).length(5).not.equal(defaultValue)
+    }
   })
 })
