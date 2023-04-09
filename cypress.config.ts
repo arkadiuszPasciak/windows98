5ceeffdd0ec2b2a221a4509eea8f7b8e8f52f2d1
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-04-09 06:48:04 +0200

    change name of job and add more default command timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
     specPattern: 'src/Bundles/**/**/Tests/components/*.components.ts',
   },
 
+  defaultCommandTimeout: 10000,
+
   e2e: {
     specPattern: 'src/Bundles/**/**/Tests/e2e/*.e2e.ts',
   },
