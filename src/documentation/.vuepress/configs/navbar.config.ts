3d5a961b388c80f5e0a79d888fd4cf03baa96677
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-04-11 05:25:30 +0200

    add page cli in docs part 1

--- src/documentation/.vuepress/configs/navbar.config.ts
+++ src/documentation/.vuepress/configs/navbar.config.ts
@@ -5,6 +5,16 @@ const navbarConfig: NavbarConfig = [
     text: 'Guide',
     link: '/guide/',
   },
+  {
+    text: 'Reference',
+    link: '/reference',
+    children: [
+      {
+        text: 'CLI',
+        link: '/reference/cli.html',
+      },
+    ],
+  },
 ]
 
 export default navbarConfig
--- src/documentation/.vuepress/configs/sidebar.config.ts
+++ src/documentation/.vuepress/configs/sidebar.config.ts
@@ -4,10 +4,14 @@ const sidebarConfig: SidebarConfig = {
   '/guide/': [
     {
       text: 'Guide',
-      children: [
-        '/guide/index.md',
-        '/guide/getting-started/manual-installation.md',
-      ],
+      children: ['/guide/index.md', '/guide/manual-installation.md'],
+    },
+  ],
+  '/reference/': [
+    {
+      text: 'Reference',
+      collapsible: true,
+      children: ['/reference/cli.md'],
     },
   ],
 }
