878628b17fd1cba7944bb8e7cbf695ae4339c5ce
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-04-29 10:28:43 +0200

    move catalog public to src/app/Public

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import Markdown from 'vite-plugin-vue-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: 'src/app/Public',
   plugins: [
     Vue({
       include: [/\.vue$/, /\.md$/], // <--
