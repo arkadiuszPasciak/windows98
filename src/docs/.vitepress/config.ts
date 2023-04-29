5887fd515f259a88815a5db1d1a87cd92e39b0d3
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-04-29 10:55:48 +0200

    change catalog name from documentations to docs

--- src/docs/.vitepress/config.ts
+++ src/docs/.vitepress/config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vitepress'
 import navbarConfig from './configs/navbar.config'
 import sidebarConfig from './configs/sidebar.config'
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: 'Windows98 Documentation',
   description: 'A VitePress Site',
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: navbarConfig,
 
     sidebar: sidebarConfig,
