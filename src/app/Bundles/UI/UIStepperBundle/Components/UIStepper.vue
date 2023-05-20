ca2d5a9f0e3c0ac8d979e3176848089e6efaaa28
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-05-20 12:16:34 +0200

    add UIStepperBundle and set in Calendar

---
+++ src/app/Bundles/UI/UIStepperBundle/Components/UIStepper.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="UIStepper">
+    <UIInput
+      v-if="fieldModification"
+      :id="input.id"
+      v-model:modelValue="value"
+      class="input"
+      :data-test="`${input.id}-input`"
+    />
+
+    <UIText v-else class="field" :data-test="`${input.id}-input`">
+      {{ value }}
+    </UIText>
+
+    <UIButton
+      class="button increase"
+      size="small"
+      :data-test="`${input.id}-increase`"
+      @click="increase"
+    />
+
+    <UIButton
+      class="button decrease"
+      size="small"
+      :data-test="`${input.id}-decrease`"
+      @click="decrease"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue'
+  import UIButton from '@APP|Bundles/UIButtonBundle/Components/UIButton.vue'
+  import UIInput from '@APP|Bundles/UIInputBundle/Components/UIInput.vue'
+  import UIText from '@APP|Bundles/UITextBundle/Components/UIText.vue'
+
+  interface UIStepperProps {
+    input: {
+      id: string
+    }
+    fieldModification: boolean
+    modelValue: number
+  }
+
+  interface UIStepperEmits {
+    (e: 'increase'): void
+    (e: 'decrease'): void
+    (e: 'value', value: number): void
+  }
+
+  const props = defineProps<UIStepperProps>()
+
+  const emit = defineEmits<UIStepperEmits>()
+
+  const value = computed({
+    get() {
+      return props.modelValue
+    },
+    set(value) {
+      emit('value', value)
+    },
+  })
+
+  const increase = (): void => {
+    emit('increase')
+  }
+
+  const decrease = (): void => {
+    emit('decrease')
+  }
+</script>
+
+<style
+  lang="scss"
+  scoped
+  src="@APP|Bundles/UIStepperBundle/Styles/UIStepper.styles.scss"
+/>
