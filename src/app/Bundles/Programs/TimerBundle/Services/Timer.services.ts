4ef9afaa3d385e7fe44f9fdada43167957df1261
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-05-25 05:39:06 +0200

    add methods to calculate time

--- src/app/Bundles/Programs/TimerBundle/Services/Timer.services.ts
+++ src/app/Bundles/Programs/TimerBundle/Services/Timer.services.ts
@@ -2,9 +2,16 @@ import {
   ETimerSwitcherMethod,
   TTimerSwitcherMethod,
 } from '@APP|Bundles/TimerBundle/Supports/TimerSwitcher.supports'
+import {
+  TTimerPresetsRadioCheck,
+  ETimerPresetsRadioCheck,
+} from '@APP|Bundles/TimerBundle/Supports/TimerPresets.supports'
 
 export class Timer {
-  public verifyTime(time: number, method: TTimerSwitcherMethod): boolean {
+  public verifySwitcherTime(
+    time: number,
+    method: TTimerSwitcherMethod,
+  ): boolean {
     if (time === 59 && method === ETimerSwitcherMethod.INCREASE) {
       return false
     }
@@ -14,5 +21,39 @@ export class Timer {
     }
 
     return true
+  }
+  public isTimerFinish(
+    hours: number,
+    minutes: number,
+    seconds: number,
+  ): boolean {
+    return hours === 0 && minutes === 0 && seconds === 0 ? true : false
+  }
+  public decreaseHoursInTimer(
+    hours: number,
+    minutes: number,
+    seconds: number,
+  ): boolean {
+    return hours >= 1 && minutes === 0 && seconds === 0 ? true : false
+  }
+  public decreaseMinutesInTimer(minutes: number, seconds: number): boolean {
+    return seconds === 0 && minutes >= 1 ? true : false
+  }
+  public updateDisplayTimesBySwitcher(
+    presets: TTimerPresetsRadioCheck,
+  ): null | number {
+    switch (presets) {
+      case ETimerPresetsRadioCheck.THREE_MINUTES:
+        return 3
+      case ETimerPresetsRadioCheck.FIVE_MINUTES:
+        return 5
+      case ETimerPresetsRadioCheck.TEN_MINUTES:
+        return 10
+      case ETimerPresetsRadioCheck.FIFTEEN_MINUTES:
+        return 15
+      case ETimerPresetsRadioCheck.CUSTOM_MINUTES:
+      default:
+        return null
+    }
   }
 }
--- src/app/Bundles/Programs/TimerBundle/Stores/Timer.stores.ts
+++ src/app/Bundles/Programs/TimerBundle/Stores/Timer.stores.ts
@@ -10,7 +10,13 @@ import {
   ETimerPresetsRadioCheck,
 } from '@APP|Bundles/TimerBundle/Supports/TimerPresets.supports'
 
-const TimerService = new Timer()
+const {
+  isTimerFinish,
+  decreaseHoursInTimer,
+  decreaseMinutesInTimer,
+  verifySwitcherTime,
+  updateDisplayTimesBySwitcher,
+} = new Timer()
 
 export const useTimerStore = defineStore('timer', {
   state: () => ({
@@ -27,7 +33,7 @@ export const useTimerStore = defineStore('timer', {
     },
     time: {
       hours: 0 as number,
-      minutes: 0 as number,
+      minutes: 3 as number,
       seconds: 0 as number,
     },
   }),
@@ -40,7 +46,7 @@ export const useTimerStore = defineStore('timer', {
         return
       }
 
-      const checkTime = TimerService.verifyTime(this.switcher[type], method)
+      const checkTime = verifySwitcherTime(this.switcher[type], method)
 
       if (!checkTime) {
         return
@@ -48,8 +54,10 @@ export const useTimerStore = defineStore('timer', {
 
       if (method === ETimerSwitcherMethod.INCREASE) {
         this.switcher[type] += 1
+        this.time[type] += 1
       } else if (method === ETimerSwitcherMethod.DECREASE) {
         this.switcher[type] -= 1
+        this.time[type] -= 1
       }
     },
     updatePresets(presets: TTimerPresetsRadioCheck): void {
@@ -60,15 +68,41 @@ export const useTimerStore = defineStore('timer', {
       this.showSwitcherByCustomPreset(presets)
 
       this.presets = presets
+
+      const updateMinutes = updateDisplayTimesBySwitcher(presets)
+      this.updateTimeByDefaultPresets(updateMinutes)
+    },
+    updateTimeByDefaultPresets(updateMinutes: null | number): void {
+      if (updateMinutes === null) {
+        this.showTimeByCustomPreset()
+        return
+      }
+
+      if (this.time.seconds !== 0) {
+        this.time.seconds = 0
+      }
+
+      if (this.time.hours !== 0) {
+        this.time.hours = 0
+      }
+
+      this.time.minutes = updateMinutes
     },
     startTime(): void {
       this.status = !this.status
 
       this.changeDisabledTime(this.status)
+
+      this.countTimer()
     },
     changeDisabledTime(status: boolean): void {
       this.disabled.presets = status
       this.disabled.switcher = status
+    },
+    showTimeByCustomPreset(): void {
+      this.time.seconds = this.switcher.seconds
+      this.time.minutes = this.switcher.minutes
+      this.time.hours = this.switcher.hours
     },
     showSwitcherByCustomPreset(presets: TTimerPresetsRadioCheck): void {
       if (presets === ETimerPresetsRadioCheck.CUSTOM_MINUTES) {
@@ -88,6 +122,37 @@ export const useTimerStore = defineStore('timer', {
     },
     resetValues(): void {
       this.resetTime()
+    },
+    countTimer(): void {
+      const interval = setInterval(() => {
+        if (
+          isTimerFinish(this.time.hours, this.time.minutes, this.time.seconds)
+        ) {
+          clearInterval(interval)
+
+          console.log('interval is clean', interval)
+          return
+        }
+
+        if (
+          decreaseHoursInTimer(
+            this.time.hours,
+            this.time.minutes,
+            this.time.seconds,
+          )
+        ) {
+          this.time.hours--
+          this.time.minutes = 59
+          this.time.seconds = 59
+        } else if (
+          decreaseMinutesInTimer(this.time.minutes, this.time.seconds)
+        ) {
+          this.time.seconds = 59
+          this.time.minutes--
+        } else {
+          this.time.seconds--
+        }
+      }, 1000)
     },
   },
 })
