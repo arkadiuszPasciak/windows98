17b9a3f2254143ccd78a433cdf79a0ae7c2fdd5e
Arkadiusz <arkadiusz.pasciak@pfizer.com>
2023-06-04 13:49:17 +0200

    set cache dir in vitest

--- src/configs/vitest.config.ts
+++ src/configs/vitest.config.ts
@@ -3,6 +3,9 @@ import { getPathMain } from './path.config'
 
 const vitestConfig = {
   globals: true,
+  cache: {
+    dir: getPathMain('node_modules/.vitest'),
+  },
   environment: 'jsdom',
   include: ['**/*.units.ts', '**/*.snapshots.ts'],
   setupFiles: [getPathMain('src/configs/vue-test-utils.config.ts')],
