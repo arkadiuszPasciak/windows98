a8a23313f84332e9472c184260a9ff1f0229dba4
Arkadiusz Paściak <a.pasciak@livespace.io>
2023-11-19 17:00:52 +0100

    add new workspace @windows98/lint-staged

--- src/configs/lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '**/*.{js,ts,vue}': ['pnpm --filter @windows98/eslint run lintfix:js'],
-  '**/*.{scss,vue}': ['pnpm --filter @windows98/stylelint run lintfix:styles'],
-  '*': ['pnpm --filter @windows98/prettier run lintfix:prettier'],
-}
