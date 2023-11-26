adc78c98ac2e654b1269f3522e278f4210d8408d
Arkadiusz Paściak <a.pasciak@livespace.io>
2023-11-26 15:39:39 +0100

    fix indent in cypress config

--- core/tests/cypress/configs/cypress.config.ts
+++ core/tests/cypress/configs/cypress.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
     indexHtmlFile: 'src/support/component-index.html',
     supportFile: 'src/support/component.ts',
-    specPattern: 
+    specPattern:
       '../../../src/app/Bundles/**/**/Tests/components/*.components.ts',
   },
 
