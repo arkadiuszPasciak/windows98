96f7e993bed365c4a868b23085a247d21daa5c66
Arkadiusz Paściak <a.pasciak@livespace.io>
2023-12-14 14:21:37 +0100

    @windows98/vitest: add workspace

---
+++ core/tests/vitest/configs/vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [tsconfigPaths({ loose: true })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['../../../**/*.units.ts'],
+  },
+})
