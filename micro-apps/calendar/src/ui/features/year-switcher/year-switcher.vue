e36664603a7bf40f82ebf5a1dc2d3f7e1a8638a5
Arkadiusz Paściak <a.pasciak@livespace.io>
2023-12-20 15:23:20 +0100

    add feature year switcher

---
+++ micro-apps/calendar/src/ui/features/year-switcher/year-switcher.vue
@@ -0,0 +1,24 @@
+<template>
+  <UIStepper
+    class="CalendarYearSwitcher"
+    :input="{ id: 'calendar-year-stepper' }"
+    :field-modification="false"
+    :model-value="year ?? 1900"
+    @increase="increaseYear"
+    @decrease="decreaseYear"
+    @value="year"
+  />
+</template>
+
+<script setup lang="ts">
+  import UIStepper from '../../../../../../app/Bundles/UI/UIStepperBundle/Components/UIStepper.vue'
+  import useYearSwitcher from './use-year-switcher.composable'
+
+  const {
+    increaseYear,
+    decreaseYear,
+    year
+  } = useYearSwitcher()
+</script>
+
+<style lang="scss" scoped />
