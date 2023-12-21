a93200a1ac94d1b9c2709bc3ec50911f593b3ef2
Arkadiusz Paściak <a.pasciak@livespace.io>
2023-12-21 16:04:56 +0100

    add feature wrapper

---
+++ micro-apps/calendar/src/ui/features/wrapper/wrapper.vue
@@ -0,0 +1,23 @@
+<template>
+	<UIFrame class="CalendarWrapper" :title="translation.title">
+	  <MonthSwitcher />
+	  <YearSwitcher />
+	  <Days />
+	</UIFrame>
+  </template>
+  
+<script setup lang="ts">
+  	import UIFrame from '@APP|Bundles/UIFrameBundle/Components/UIFrame.vue'
+	import Days from '../days/days.vue'
+	import YearSwitcher from '../year-switcher/year-switcher.vue'
+	import MonthSwitcher from '../month-switcher/month-switcher.vue'
+	import useWrapper from './use-wrapper.composable'
+
+	const { translation } = useWrapper()
+</script>
+  
+<style
+	lang="scss"
+	scoped
+	src="./wrapper.style.scss"
+/>
