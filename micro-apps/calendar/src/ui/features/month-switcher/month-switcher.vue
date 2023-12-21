239a604a15e76a3fbc5c92406e9eef5dd2e5b24d
Arkadiusz Paściak <a.pasciak@livespace.io>
2023-12-21 15:14:51 +0100

    add feature month switcher

---
+++ micro-apps/calendar/src/ui/features/month-switcher/month-switcher.vue
@@ -0,0 +1,26 @@
+<template>
+  <UISelect
+    id="date-months"
+    class="CalendarMonthSwitcher"
+    :model-value="month"
+    :options="selectMonths"
+    @update:model-value="changeCalendarByMonth($event)"
+  />
+</template>
+
+<script setup lang="ts">
+  import UISelect from '@APP|Bundles/UISelectBundle/Components/UISelect.vue'
+  import useMonthSwitcher from './use-month-switcher.composable'
+
+  const {
+    changeCalendarByMonth,
+		month,
+		selectMonths
+  } = useMonthSwitcher()
+</script>
+
+<style
+  lang="scss"
+  scoped
+  src="./month-switcher.style.scss"
+/>
