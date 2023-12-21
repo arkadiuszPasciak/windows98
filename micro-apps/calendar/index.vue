bf917ce7c80464483f11fd50a4b0aa0f4952cc4c
Arkadiusz Paściak <a.pasciak@livespace.io>
2023-12-21 16:10:51 +0100

    add index export micro-app

---
+++ micro-apps/calendar/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<CalendarView />
+  </template>
+  
+<script setup lang="ts">
+  	import CalendarView from './src/ui/views/calendar.view.vue'
+</script>
+  
+<style
+	lang="scss"
+	scoped
+/>
---
+++ micro-apps/calendar/src/ui/views/calendar.view.vue
@@ -0,0 +1,12 @@
+<template>
+	<CalendarWrapper />
+  </template>
+  
+<script setup lang="ts">
+  	import CalendarWrapper from '../features/wrapper/wrapper.vue'
+</script>
+  
+<style
+	lang="scss"
+	scoped
+/>
