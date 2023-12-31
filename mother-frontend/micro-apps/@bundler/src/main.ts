cc6c691db5ae024a7bc743c5656a01de110afc5d
Arkadiusz Paściak <a.pasciak@livespace.io>
2023-12-31 13:24:48 +0100

    @windows98/micro-apps: add @bundler prototype part 4

--- mother-frontend/micro-apps/@bundler/src/main.ts
+++ mother-frontend/micro-apps/@bundler/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import vueI18n from '@windows98/i18n/configs/vue.config'
 import Calendar from '../../calendar/src/ui/views/calendar.view.vue'
+import '@windows98/design-system/configs/base.config.scss?inline'
 
 const app = createApp(Calendar)
 
