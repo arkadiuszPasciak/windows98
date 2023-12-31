c4254d685bd9ecf84c0bfcef7032ae81fc33aba9
Arkadiusz Paściak <a.pasciak@livespace.io>
2023-12-31 13:15:33 +0100

    @windows98/micro-apps: add @bundler prototype part 3

--- mother-frontend/micro-apps/@bundler/src/main.ts
+++ mother-frontend/micro-apps/@bundler/src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import vueI18n from '@windows98/i18n/configs/vue.config'
 import Calendar from '../../calendar/src/ui/views/calendar.view.vue'
 
-createApp(Calendar).mount('#app')
+const app = createApp(Calendar)
+
+app.use(vueI18n)
+app.mount('#app')
--- mother-frontend/micro-apps/@bundler/vite.config.ts
+++ mother-frontend/micro-apps/@bundler/vite.config.ts
@@ -3,5 +3,14 @@ import Vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import '@windows98/design-system/configs/index.config.scss';
+        `,
+      },
+    },
+  },
   plugins: [Vue(), tsconfigPaths()],
 })
