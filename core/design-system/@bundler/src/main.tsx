c78d7cd2b78041ba86d68f500290fccf7feaa453
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-01-13 13:23:49 +0100

    @windows98/design-system: inject global styles

--- core/design-system/@bundler/src/main.tsx
+++ core/design-system/@bundler/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Test from '../../src/components/test'
 import './styles/main.scss'
+import '../../configs/base.config.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- core/design-system/@bundler/vite.config.ts
+++ core/design-system/@bundler/vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import '../configs/index.config.scss';
+        `,
+      },
+    },
+  },
   plugins: [react()],
 })
