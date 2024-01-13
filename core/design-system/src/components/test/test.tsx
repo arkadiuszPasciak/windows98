8c91ee7598c6a61c0769f53358087f8f30bec043
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-01-13 13:17:30 +0100

    @windows98/design-system: add css modules

--- core/design-system/src/components/test/test.tsx
+++ core/design-system/src/components/test/test.tsx
@@ -1,5 +1,5 @@
 import useTest from './use-test.hook'
-import './test.scss'
+import styles from './test.module.scss'
 
 export default function Test(): JSX.Element {
   const { count, setCountByOne } = useTest()
@@ -8,20 +8,20 @@ export default function Test(): JSX.Element {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={''} className="logo" alt="Vite logo" />
+          <img src={''} className={styles["logo"]} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={''} className="logo react" alt="React logo" />
+          <img src={''} className={`${styles["logo"]} ${styles["react"]}`} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className={styles['card']}>
         <button onClick={() => setCountByOne()}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className={styles['read-the-docs']}>
         Click on the Vite and React logos to learn more
       </p>
     </>
