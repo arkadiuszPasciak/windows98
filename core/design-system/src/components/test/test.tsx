71175944ff60329adfddd64b28e31b66f099ceb5
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-01-14 15:16:41 +0100

    @windows98/design-system: install r2wc part 2

--- core/design-system/src/components/test/test.tsx
+++ core/design-system/src/components/test/test.tsx
@@ -8,10 +8,14 @@ export default function Test(): JSX.Element {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={''} className={styles["logo"]} alt="Vite logo" />
+          <img src={''} className={styles['logo']} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={''} className={`${styles["logo"]} ${styles["react"]}`} alt="React logo" />
+          <img
+            src={''}
+            className={`${styles['logo']} ${styles['react']}`}
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
--- core/design-system/src/components/test/test.wc.ts
+++ core/design-system/src/components/test/test.wc.ts
@@ -1,6 +1,6 @@
-import r2wc from "@r2wc/react-to-web-component"
-import Test from "./test"
+import r2wc from '@r2wc/react-to-web-component'
+import Test from './test'
 
 const DesignSystemTest = r2wc(Test)
 
-customElements.define("ds-test", DesignSystemTest)
+customElements.define('ds-test', DesignSystemTest)
