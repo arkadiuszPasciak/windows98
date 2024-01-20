07f93bd9e73f87a491a933fdcaf2253338d2d4ef
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-01-20 11:28:20 +0100

    @windows98/design-system: add playwright

---
+++ core/design-system/src/components/test/test.test.tsx
@@ -0,0 +1,12 @@
+import { expect, test } from "@playwright/experimental-ct-react"
+import Test from "./index"
+
+test.use({ viewport: { width: 500, height: 500 } })
+
+test.describe("Test", () => {
+	test("renders properly", async ({ mount }) => {
+		const component = await mount(<Test></Test>)
+
+		await expect(component).toBeVisible()
+	})
+})
