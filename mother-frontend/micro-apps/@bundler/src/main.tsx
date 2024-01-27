413eae584f465f966de38dff42bdf47e57206c22
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-01-27 14:13:27 +0100

    feat(ma): change ui framework from react to vue

---
+++ mother-frontend/micro-apps/@bundler/src/main.tsx
@@ -0,0 +1,12 @@
+// import Calendar from '../../calendar/src/ui/views/calendar.view.vue'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '@windows98/design-system/base.config.css'
+// import { Test } from '@windows98/design-system'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* <Test>123</Test> */}
+    <p>123</p>
+  </React.StrictMode>,
+)
--- mother-frontend/micro-apps/@bundler/vite.config.ts
+++ mother-frontend/micro-apps/@bundler/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   css: {
@@ -12,11 +11,5 @@ export default defineConfig({
       },
     },
   },
-  define: {
-    __VUE_I18N_FULL_INSTALL__: false,
-    __VUE_I18N_LEGACY_API__: false,
-    __INTLIFY_PROD_DEVTOOLS__: false,
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
-  },
-  plugins: [Vue(), tsconfigPaths()],
+  plugins: [react()],
 })
