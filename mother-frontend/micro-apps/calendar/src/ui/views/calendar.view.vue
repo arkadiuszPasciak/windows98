3e60c0c3b8d9b8f2acdfffc2fedfbded521ab988
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-04 14:44:04 +0100

    fix(ma): calendar - change name of component structure in ui domain

--- mother-frontend/micro-apps/calendar/src/ui/views/calendar.view.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <CalendarWrapper />
-</template>
-
-<script setup lang="ts">
-  import CalendarWrapper from '../features/wrapper/wrapper.vue'
-</script>
-
-<style lang="scss" scoped />
