f58d797d4fa578cd21477c13770d19fbbb182ad3
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-10 16:52:09 +0100

    feat(ds): ds-button-arrow - add props onClick and remove icon

--- core/design-system/src/components/ds-button-arrow/ds-button-arrow.tsx
+++ core/design-system/src/components/ds-button-arrow/ds-button-arrow.tsx
@@ -1,20 +1,20 @@
-import { DSIcon } from '../..';
 import type { DSButtonArrowProps } from './ds-button-arrow.type';
 
 export function DSButtonArrow({
 	ariaLabel,
 	direction,
 	disabled,
+	onClick,
 	size
 }: DSButtonArrowProps) {
-	<button
-		aria-label={ariaLabel}
-		className={`ds-button-arrow ${direction} ${size}`}
-		disabled={disabled}
-		type="button"
-	>
-		<DSIcon
-			name="select-button"
+	return (
+		<button
+			aria-label={ariaLabel}
+			className={`ds-button-arrow ${direction} ${size}`}
+			disabled={disabled}
+			type="button"
+			onClick={onClick}
 		/>
-	</button>
+	)
+	
 }
--- core/design-system/src/components/ds-button-arrow/ds-button-arrow.type.ts
+++ core/design-system/src/components/ds-button-arrow/ds-button-arrow.type.ts
@@ -1,6 +1,9 @@
+import { MouseEvent } from 'react';
+
 export interface DSButtonArrowProps {
 	ariaLabel: string
 	direction: 'top' | 'bottom'
 	disabled: boolean
+	onClick?: (event: MouseEvent<HTMLButtonElement>) => void
 	size: 'small' | 'medium'
 }
