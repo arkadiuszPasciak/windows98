d62a75357785fb2804047141cc0e23101e04ba03
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-11 15:08:13 +0100

    stories(ds): ds-stepper

--- core/design-system/src/components/ds-stepper/ds-stepper.stories.tsx
+++ core/design-system/src/components/ds-stepper/ds-stepper.stories.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import { DSStepper } from '../../src'
+import { DSStepper } from './index'
 
-export function TestComponent(): JSX.Element {
-	const [value, setValue] = useState(0)
+export function DSStepperStories(): JSX.Element {
+	const [value, setValue] = useState<number>(0)
 
 	return (
 		<DSStepper
-			id="test"
+			id="stepper"
 			modelValue={value}
 			onIncrease={() => setValue(value + 1)}
 			onDecrease={() => setValue(value - 1)}
 		/>
-  	)	
+  	)
 }
