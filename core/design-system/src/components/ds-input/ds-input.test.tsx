913ae40966f8ebcf44e3536c46a596142ce40a8b
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-13 09:05:06 +0100

    test(ds): ds-input

---
+++ core/design-system/src/components/ds-input/ds-input.test.tsx
@@ -0,0 +1,53 @@
+import { expect, test } from '@playwright/experimental-ct-react'
+import { DSInput, type DSInputProps } from './index'
+
+const defaultInput: DSInputProps = {
+  id: 'input',
+  type: 'text',
+  disabled: false,
+  readonly: false,
+  initialValue: '',
+  labelName: 'Test Label',
+  wrapperLayout: 'vertical',
+  onChange: () => {},
+}
+
+const disabledInput: DSInputProps = {
+  ...defaultInput,
+  disabled: true,
+}
+
+test.use({ viewport: { width: 500, height: 500 } })
+
+test.describe('DSInput', () => {
+  test('renders properly', async ({ mount }) => {
+    const component = await mount(<DSInput {...defaultInput} />)
+
+    const inputElement = await component.getByTestId(`ds-input-input-${defaultInput.id}`)
+    const labelElement = await component.getByTestId(`ds-input-label-${defaultInput.id}`)
+
+    await expect(component).toBeVisible()
+    await expect(inputElement).toBeVisible()
+    await expect(labelElement).toBeVisible()
+  })
+
+  test('handles input change', async ({ mount }) => {
+    const component = await mount(<DSInput {...defaultInput} />)
+
+    const inputElement = await component.getByTestId(`ds-input-input-${defaultInput.id}`)
+
+    await inputElement.type('Test Input')
+
+    await expect(inputElement).toHaveValue('Test Input')
+  })
+
+  test('does not allow input when disabled', async ({ mount }) => {
+    const component = await mount(<DSInput {...disabledInput} />)
+
+    const inputElement = await component.getByTestId(`ds-input-input-${defaultInput.id}`)
+
+    const isInputDisabled = await inputElement.isDisabled()
+
+    await expect(isInputDisabled).toBe(true)
+  })
+})
