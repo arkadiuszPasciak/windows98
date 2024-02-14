20515f4ffa388fd2bad411c537d4d0fef7fd76f8
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-14 08:29:54 +0100

    style(ma): calendar - wrapper and add todo

--- mother-frontend/micro-apps/calendar/src/ui/modules/wrapper/wrapper.tsx
+++ mother-frontend/micro-apps/calendar/src/ui/modules/wrapper/wrapper.tsx
@@ -1,8 +1,21 @@
 import MonthSwitcher from '../month-switcher/month-switcher'
+import styles from './wrapper.module.scss'
+
+// TODO
+// 1. add component:
+{/* <UIFrame class="CalendarWrapper" :title="translation.title"> -->
+<MonthSwitcher />
+<YearSwitcher />
+<Days />
+</UIFrame> */}
+// 2. init domain here
+// import useWrapper from './use-wrapper.composable'
+// const { initCalendar, translation } = useWrapper()
+// initCalendar()
 
 export function Wrapper() {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <MonthSwitcher />
     </div>
   )
