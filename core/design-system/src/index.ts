1e81029e87b48b77cf51ccf337992ead11b314f1
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-15 08:18:59 +0100

    feat(ds): ds-frame - export index

--- core/design-system/src/index.ts
+++ core/design-system/src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/ds-button-arrow'
+export * from './components/ds-frame'
 export * from './components/ds-icon'
 export * from './components/ds-input'
 export * from './components/ds-select'
