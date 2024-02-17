a79c445e58330fc2b6f69be769c5e307a77a640a
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-17 13:17:34 +0100

    style(ma): calendar - days

--- mother-frontend/micro-apps/calendar/src/ui/modules/days/days.tsx
+++ mother-frontend/micro-apps/calendar/src/ui/modules/days/days.tsx
@@ -1,9 +1,10 @@
 import { WeekDays } from './week-days/week-days'
 import { MonthDays } from './month-days/month-days'
+import styles from './days.module.scss'
 
 export function Days() {
   return (
-    <div className="days">
+    <div className={styles['days']}>
       <WeekDays />
       <MonthDays />
     </div>
