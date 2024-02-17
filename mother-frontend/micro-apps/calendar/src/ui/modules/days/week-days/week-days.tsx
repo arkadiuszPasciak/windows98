a2ba615ebeca451b0acd9280eb399d5c8a356a62
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-17 12:46:13 +0100

    style(ma) calendar - week days

--- mother-frontend/micro-apps/calendar/src/ui/modules/days/week-days/week-days.tsx
+++ mother-frontend/micro-apps/calendar/src/ui/modules/days/week-days/week-days.tsx
@@ -1,12 +1,13 @@
 import { useWeekDays } from './use-week-days.hook'
+import styles from './week-days.module.scss'
 
 export function WeekDays() {
 	const { dictionary } = useWeekDays()
 
 	return (
-		<div className="week-days">
+		<div className={styles['week-days']}>
 			{dictionary.map((day, id) => (
-				<span className="day" data-testid={`calendar-week-days-day-${id}`}>
+				<span className={styles['day']} data-testid={`calendar-week-days-day-${id}`}>
 					{day}
 				</span>
 			))}
