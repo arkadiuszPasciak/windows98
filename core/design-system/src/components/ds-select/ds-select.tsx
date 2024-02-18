f5764d67b1f8fb3c06d23813f5b25b47dfe22bca
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-18 15:38:07 +0100

    fix(ds): ds-select - arrow icon

--- core/design-system/src/components/ds-select/ds-select.tsx
+++ core/design-system/src/components/ds-select/ds-select.tsx
@@ -11,7 +11,6 @@ export function DSSelect({
   modelValue,
   options,
   labelName,
-  labelPosition = 'top',
   onSelect,
 }: DSSelectProps) {
   const { value, handleChange } = useDSSelect(modelValue)
@@ -24,7 +23,7 @@ export function DSSelect({
   }
 
   return (
-    <div className={`${styles[labelName ? `label-position-${labelPosition}` : '']} ${className}`}>
+    <div className={`${styles['wrapper']} ${className}`}>
       {labelName && (
         <label
           className={styles['label']}
