ceee9c4b8ae9d5c2935b7bc114ce7c6286fbb058
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-18 14:17:38 +0100

    feat(ma): calendar - use context in view

--- mother-frontend/micro-apps/calendar/src/ui/views/calendar.view.tsx
+++ mother-frontend/micro-apps/calendar/src/ui/views/calendar.view.tsx
@@ -1,5 +1,11 @@
+import { DomainContext } from '../context/domain.context'
 import { Wrapper } from '../modules/wrapper/wrapper'
+import { calendarService } from '../../domain/services/calendar.service'
 
 export const CalendarView = () => {
-  return <Wrapper />
+  return (
+    <DomainContext.Provider value={calendarService}>
+      <Wrapper />
+    </DomainContext.Provider>
+  )
 }
