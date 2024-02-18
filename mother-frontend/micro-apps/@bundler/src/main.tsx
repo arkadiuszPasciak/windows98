538ef61b7abf837d88e32a728752bff1be38076f
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-18 16:17:50 +0100

    feat(ma): all - init i18n

--- mother-frontend/micro-apps/@bundler/src/main.tsx
+++ mother-frontend/micro-apps/@bundler/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import '@windows98/design-system/base.config.css'
 import '@windows98/design-system/variables.config.css'
 import { CalendarView } from '../../calendar/src/ui/views/calendar.view'
+import i18nInit from '@windows98/i18n/configs/react.config'
+
+i18nInit()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- mother-frontend/micro-apps/@bundler/vite.config.ts
+++ mother-frontend/micro-apps/@bundler/vite.config.ts
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      external: ['@windows98/i18n', 'i18next', 'react-i18next'],
+      output: {
+        globals: {
+          '@windows98/i18n': 'i18nInit',
+          'react-i18next': 'reactI18next',
+          i18next: 'i18next',
+        },
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
