21bdd3fbe5d0d5bdd072d3eaccb612b693343f72
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-21 09:24:02 +0100

    test(ma): calendar - frame has correct text and select month

--- mother-frontend/micro-apps/calendar/tests/integrations/calendar.test.tsx
+++ mother-frontend/micro-apps/calendar/tests/integrations/calendar.test.tsx
@@ -3,12 +3,28 @@ import { CalendarView } from '../../src/ui/views/calendar.view'
 
 test.use({ viewport: { width: 500, height: 500 } })
 
-test.describe('Calendar', () => {
+test.describe('calendar', () => {
   test('renders properly', async ({ mount }) => {
-    const component = await mount(
-      <CalendarView />,
-    )
-
+    const component = await mount(<CalendarView />)
     await expect(component).toBeVisible()
+  });
+
+  test('frame has correct text', async ({ mount }) => {
+    const component = await mount(<CalendarView />)
+    const frame = await component.getByTestId('ds-text-frame-calculator')
+    
+    await expect(frame).toHaveText('Calendar')
+  })
+
+  test('select month', async ({ mount }) => {
+    const component = await mount(<CalendarView />)
+    const select = await component.getByTestId('ds-select-select-month-switcher')
+
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+    for (let index = 0; index <= 11; index++) {
+      await select.selectOption(months[index])
+      await expect(select).toHaveValue(String(index))
+    }
   })
 })
