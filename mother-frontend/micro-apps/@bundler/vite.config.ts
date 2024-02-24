65d72001674897e3b87cce6866232ebef9ab9e7e
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-24 14:22:57 +0100

    feat(ma): change workspace from app to library

--- mother-frontend/micro-apps/@bundler/vite.config.ts
+++ mother-frontend/micro-apps/@bundler/vite.config.ts
@@ -3,12 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   build: {
+    copyPublicDir: false,
+    lib: {
+      entry: '../calendar/calendar.wc.ts',
+      name: 'micro-apps',
+      fileName: 'index',
+    },
     emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
-      external: ['@windows98/i18n', 'i18next', 'react-i18next'],
+      external: ['react', 'react-dom', '@windows98/i18n', 'i18next', 'react-i18next'],
       output: {
         globals: {
+          react: 'react',
+          'react-dom': 'reactDOM',
           '@windows98/i18n': 'i18nInit',
           'react-i18next': 'reactI18next',
           i18next: 'i18next',
