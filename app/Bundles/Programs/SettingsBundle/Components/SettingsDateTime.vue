9e67d78af23336b29e6e7acac3510fa1a7a18833
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-26 15:39:19 +0100

    fix(app): delete commented import old calendar

--- app/Bundles/Programs/SettingsBundle/Components/SettingsDateTime.vue
+++ app/Bundles/Programs/SettingsBundle/Components/SettingsDateTime.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="SettingsDateTime">
-    <!-- <CalendarSettings /> -->
     <ma-calendar></ma-calendar>
     <TimezoneSwitcher />
   </div>
 </template>
 
 <script setup lang="ts">
-  // import CalendarSettings from '@APP|Bundles/CalendarBundle/Components/CalendarSettings.vue'
   import TimezoneSwitcher from '@APP|Bundles/TimezoneBundle/Components/TimezoneSwitcher.vue'
 
   import '@windows98/micro-apps'
