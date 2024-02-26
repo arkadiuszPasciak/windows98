15ad941e0c86278c5aa24c0d270ef5605c817ae5
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-26 16:00:54 +0100

    fix(app): add needed css variable

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import mountApp from '@APP|Bundles/AppBundle/Services/App.services'
 import '@windows98/design-system/base.config.css'
+import '@windows98/design-system/variables.config.css'
 
 mountApp()
