bf86ce6d7ed1e0f2a7632e8bf2ed7bd132f2bc35
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-02-29 11:19:42 +0100

    fix(app): update vite config and delete vite eslint plugin

--- app/configs/vite.config.ts
+++ app/configs/vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-import eslintPlugin from 'vite-plugin-eslint'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import vitestConfig from './vitest.config'
 import {
   isEnvironment,
   EEnvironment,
 } from '../../src/configs/environment.config'
 import { getPathMain } from '../../src/configs/path.config'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: getPathMain('dist'),
@@ -40,12 +37,6 @@ export default defineConfig({
     VueI18nPlugin({
       strictMessage: false,
     }),
-    eslintPlugin({
-      overrideConfigFile: getPathMain(
-        'core/linters/eslint/configs/eslint.config.js',
-      ),
-    }),
     tsconfigPaths({ loose: true }),
   ],
-  test: vitestConfig,
 })
