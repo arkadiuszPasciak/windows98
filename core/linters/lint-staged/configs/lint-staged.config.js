52ac66978657648add69cf400e3dcd0aca64188d
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-03-01 09:42:28 +0100

    fix(lint-staged): command cli

--- core/linters/lint-staged/configs/lint-staged.config.js
+++ core/linters/lint-staged/configs/lint-staged.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  '../../../**/*.{js,ts,vue}': [
+  '../../../app/**/*.{js,ts,vue}': [
     'pnpm --filter @windows98/eslint run lintfix:js',
   ],
-  '../../../**/*.{scss,vue}': [
+  '../../../app/**/*.{scss,vue}': [
     'pnpm --filter @windows98/stylelint run lintfix:styles',
   ],
-  '../../../*': ['pnpm --filter @windows98/prettier run lintfix:prettier'],
+  '../../../app/*': ['pnpm --filter @windows98/prettier run lintfix:prettier'],
 }
