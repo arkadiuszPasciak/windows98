0520da7a1debd6f42a25188646c443f8867eb54b
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-03-02 11:37:49 +0100

    refactor(linters): eslint - add base settings

---
+++ core/linters/eslint/configs/base.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2022: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+	],
+	ignorePatterns: [
+		".cache",
+		"dist",
+		"node_modules",
+		"test-results",
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	rules: {
+		indent: [
+			"error",
+			"tab",
+		],
+		"jsx-quotes": [
+			"error",
+			"prefer-double",
+		],
+		semi: [
+			"error",
+			"never",
+		],
+		quotes: [
+			"error",
+			"double",
+		],
+	}
+}
