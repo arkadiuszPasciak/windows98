e790b4c1f63010460854f19b7194a7d2f1ec2936
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-03-03 12:50:28 +0100

    feat(eslint-config): add new typescript config

---
+++ core/linters/eslint/src/configs/typescript.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: [
+        "./base.config.js",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+}
