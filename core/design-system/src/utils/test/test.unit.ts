817a77f52c0360309632e92f710d3489560ddace
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-03-16 13:29:16 +0100

    feat(ds): install @windows98/configs, extend vitest shared config, add test util

---
+++ core/design-system/src/utils/test/test.unit.ts
@@ -0,0 +1,6 @@
+import { expect, describe } from "vitest"
+import test from "./test"
+
+describe("empty test to run vitest unit test, to delete when will be real test", () => {
+	expect(test(2, 2)).toBe(4)
+})
