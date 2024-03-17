71ed1a9ec030527b0dc4daab5cc64d3af07ebf79
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-03-17 09:19:43 +0100

    feat(configs): install @playwright/experimental-ct-react, add a configuration file and export it

---
+++ core/configs/src/configs/playwright/component-react.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig, devices } from "@playwright/experimental-ct-react"
+
+export default defineConfig({
+	snapshotDir: "./snapshots",
+	timeout: 10 * 1000,
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	outputDir: "./test-results",
+	reporter: [["list"], ["json", { outputFile: "./test-results/results.json" }]],
+	use: {
+		trace: "on-first-retry",
+		ctPort: 3100,
+		ctViteConfig: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: `
+							@import '@windows98/design-system/index.config.scss';
+						`,
+					},
+				},
+			},
+		},
+	},
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+		{
+			name: "firefox",
+			use: { ...devices["Desktop Firefox"] },
+		},
+		{
+			name: "webkit",
+			use: { ...devices["Desktop Safari"] },
+		},
+	],
+})
