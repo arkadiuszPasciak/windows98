1304c2d465c293c674dad2f24b4ffaa5389b0098
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-03-17 09:30:42 +0100

    fix(ds): playwright - add a block use

--- core/design-system/@test/playwright/playwright/index.tsx
+++ core/design-system/@test/playwright/playwright/index.tsx
@@ -1,3 +1,3 @@
-import '../../../src/styles/configs/reset.config.css'
-import '../../../src/styles/configs/base.config.css'
-import '../../../@bundler/dist/styles/variables.config.css'
+import "../../../src/styles/configs/reset.config.css"
+import "../../../src/styles/configs/base.config.css"
+import "../../../@bundler/build/styles/variables.config.css"
