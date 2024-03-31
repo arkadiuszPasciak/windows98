b8230b47dd4d8692644654d134c0432e0a099885
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-03-31 16:22:57 +0200

    fix(ds): standarize tokens config object

--- core/design-system/@styles/src/themes/tokens.config.ts
+++ core/design-system/@styles/src/themes/tokens.config.ts
@@ -3,76 +3,36 @@ import type { Config } from "@pandacss/dev"
 export const customTokens: Config["theme"] = {
 	tokens: {
 		colors: {
-			grey: {
-				25: { value: "#dfdfdf" },
-				100: { value: "#b5b5b5" }
-			},
-			// "grey-25": {
-			// 	value: "#dfdfdf"
-			// },
-			"grey-50": {
-				value: "#c0c0c0"
-			},
-			// "grey-100": {
-			// 	value: "#b5b5b5"
-			// },
-			"grey-200": {
-				value: "#a9a9a9"
-			},
-			"grey-300": {
-				value: "#9b9b9b"
-			},
-			"grey-400": {
-				value: "#969696"
-			},
-			"grey-500": {
-				value: "#8b8b8b"
-			},
-			"grey-600": {
-				value: "#808080"
-			},
-			"grey-700": {
-				value: "#3b3b3b"
-			},
 			black: {
 				800: { value: "#222222" },
+				900: { value: "#0a0a0a" },
+				1000: { value: "#000000" },
 			},
-			// "black-800": {
-			// 	value: "#222222"
-			// },
-			"black-900": {
-				value: "#0a0a0a"
+			blue: {
+				400: { value: "#0000ff" },
+				500: { value: "#330099" },
+				600: { value: "#1084d0" },
+				700: { value: "#0f0f81" },
+				800: { value: "#007b7b" },
+				900: { value: "#000080" },
 			},
-			"black-1000": {
-				value: "#000000"
+			grey: {
+				25: { value: "#dfdfdf" },
+				50: { value: "#c0c0c0" },
+				100: { value: "#b5b5b5" },
+				200: { value: "#a9a9a9" },
+				300: { value: "#9b9b9b" },
+				400: { value: "#969696" },
+				500: { value: "#8b8b8b" },
+				600: { value: "#808080" },
+				700: { value: "#3b3b3b" },
 			},
-			"blue-400": {
-				value: "#0000ff"
-			},
-			"blue-500": {
-				value: "#330099"
-			},
-			"blue-600": {
-				value: "#1084d0"
-			},
-			"blue-700": {
-				value: "#0f0f81"
-			},
-			"blue-800": {
-				value: "#007b7b"
-			},
-			"blue-900": {
-				value: "#000080"
-			},
-			"red-500": {
-				value: "#fe0000"
+			red: {
+				500: { value: "#fe0000" },
 			},
 			white: {
 				1000: { value: "#ffffff" },
 			}
-			// "white-1000": {
-			// 	value: "#ffffff"
-			// }
 		}
 	}
 }
