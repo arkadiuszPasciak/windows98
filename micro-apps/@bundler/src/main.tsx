123799d470f7fba8e38d19d3b83695549c25ff22
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-04-03 13:49:27 +0200

    feat(ma): add catalog src in workspace root, move calendar to src and change name calendar to ma-calendar

--- micro-apps/@bundler/src/main.tsx
+++ micro-apps/@bundler/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "@windows98/design-system/base.config.css"
 import "@windows98/design-system/variables.config.css"
-import { CalendarView } from "../../calendar/src/ui/views/calendar.view"
+import { CalendarView } from "../../src/ma-calendar/src/ui/views/calendar.view"
 import i18nInit from "@windows98/i18n/configs/react.config"
 
 i18nInit().then(() => {
--- micro-apps/@bundler/src/wc/index.wc.ts
+++ micro-apps/@bundler/src/wc/index.wc.ts
@@ -1,6 +1,6 @@
 import r2wc from "@r2wc/react-to-web-component"
 import i18nInit from "@windows98/i18n/configs/react.config"
-import { CalendarView } from "../../../calendar/src/ui/views/calendar.view"
+import { CalendarView } from "../../../src/ma-calendar/src/ui/views/calendar.view"
 
 const MACalendar = r2wc(CalendarView)
 customElements.define("ma-calendar", MACalendar)
