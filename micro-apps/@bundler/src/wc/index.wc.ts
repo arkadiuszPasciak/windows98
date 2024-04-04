55abaafb4d4f0e60bc4c859468d8d91e5b63c3ab
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-04-04 12:48:00 +0200

    fix(ma): timezone - eslint errors

--- micro-apps/@bundler/src/wc/index.wc.ts
+++ micro-apps/@bundler/src/wc/index.wc.ts
@@ -1,7 +1,7 @@
 import r2wc from "@r2wc/react-to-web-component"
 import i18nInit from "@windows98/i18n/configs/react.config"
 import { CalendarView } from "../../../src/ma-calendar/src/ui/views/calendar.view"
-import { TimezoneView } from '../../../src/ma-timezone/src/ui/views/timezone.view'
+import { TimezoneView } from "../../../src/ma-timezone/src/ui/views/timezone.view"
 
 const MACalendar = r2wc(CalendarView)
 customElements.define("ma-calendar", MACalendar)
