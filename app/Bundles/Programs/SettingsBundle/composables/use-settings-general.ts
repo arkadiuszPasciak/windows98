e753e4d11e899841dd053e026370b4e4f97e9024
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-04-05 13:58:44 +0200

    fix(app): import of user agent from micro service

--- app/Bundles/Programs/SettingsBundle/composables/use-settings-general.ts
+++ app/Bundles/Programs/SettingsBundle/composables/use-settings-general.ts
@@ -1,7 +1,7 @@
-import { UserAgentMicroServices } from "@windows98/micro-services/src/ms-user-agent"
+import { MSUserAgent } from "@windows98/micro-services/@bundler/src/main"
 
 export default function useSettingsGeneral() {
-	const userAgentMicroServices = new UserAgentMicroServices()
+	const userAgentMicroServices = new MSUserAgent()
 
 	const browserName = userAgentMicroServices.getBrowserName()
 	const deviceType = userAgentMicroServices.getTypeDevice()
