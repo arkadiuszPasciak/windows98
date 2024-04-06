3ba65feecc0fd521dbc606a04ce1d104f90a673a
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-04-06 11:08:46 +0200

    feat(ms): database - add contract and repository

---
+++ micro-services/src/ms-database/src/data/repositories/database.repository.ts
@@ -0,0 +1,52 @@
+import type { Maybe } from "@windows98/toolkit/src/types"
+import type { DatabaseRepositoryContract } from "../../domain/contracts/repository.contract"
+
+export class DatabaseRepository implements DatabaseRepositoryContract {
+	private database: Maybe<IDBDatabase> = null
+
+	constructor(
+		private databaseName: string,
+		private storeName: string
+	) { }
+
+	async open(): Promise<void> {
+		return new Promise((resolve, reject) => {
+			const request = indexedDB.open(this.databaseName)
+
+			request.onerror = () => reject(request.error)
+			request.onsuccess = () => {
+				this.database = request.result
+				resolve()
+			}
+			request.onupgradeneeded = (event) => {
+				this.database = (event.target as IDBOpenDBRequest).result
+				this.database.createObjectStore(this.storeName)
+			}
+		})
+	}
+
+	private getStore(mode: IDBTransactionMode): IDBObjectStore {
+		if (!this.database) {
+			throw new Error("Database not open")
+		}
+
+		const transaction = this.database.transaction(this.storeName, mode)
+		return transaction.objectStore(this.storeName)
+	}
+
+	public async add<T>(item: T): Promise<void> {
+		this.getStore("readwrite").add(item)
+	}
+
+	public async get<T>(key: IDBValidKey): Promise<IDBRequest<T>> {
+		return this.getStore("readonly").get(key)
+	}
+
+	public async update<T>(key: IDBValidKey, item: T): Promise<void> {
+		this.getStore("readwrite").put(item, key)
+	}
+
+	public async delete(key: IDBValidKey): Promise<void> {
+		this.getStore("readwrite").delete(key)
+	}
+}
