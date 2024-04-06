bd04053083295c8c7154b601c81479855450cb4e
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-04-06 09:46:19 +0200

    refactor(ms): browser-env - add screen repository

--- micro-services/src/ms-user-agent/src/domain/contracts/service.contract.ts
+++ micro-services/src/ms-user-agent/src/domain/contracts/service.contract.ts
@@ -2,9 +2,11 @@ import type { Maybe } from "@windows98/toolkit/src/types"
 import type { TBrowserName } from "../models/browser.model"
 import type { TDevices } from '../models/device.model'
 import type { TOperationSystemAll } from '../models/operation.model'
+import type { IScreenInformation } from '../models/screen.model'
 
 export default interface BrowserEnvServiceContract {
   getBrowserName(): Maybe<TBrowserName>
   getDeviceType(): Maybe<TDevices>
   getOperationSystem(): Maybe<TOperationSystemAll>
+  getScreenInformation(): Maybe<IScreenInformation>
 }
--- micro-services/src/ms-user-agent/src/domain/services/browser-env.service.ts
+++ micro-services/src/ms-user-agent/src/domain/services/browser-env.service.ts
@@ -1,29 +1,30 @@
-import type { Maybe } from "@windows98/toolkit/src/types"
-import type { TBrowserName } from "../models/browser.model"
-import type { TDevices } from '../models/device.model'
-import type { TOperationSystemAll } from '../models/operation.model'
 import type BrowserEnvServiceContract from '../contracts/service.contract'
 import BrowserNameRepository from "../../data/repositories/browser-name.repository"
 import DeviceTypeRepository from '../../data/repositories/device-type.repository'
 import OperationSystemRepository from '../../data/repositories/operation-system.repository'
+import ScreenRepository from '../../data/repositories/screen.repository'
 
-// TODO - add contract
 export default class BrowserEnvService implements BrowserEnvServiceContract {
 	constructor(
 		private readonly browserNameRepository = new BrowserNameRepository(),
 		private readonly deviceTypeRepository = new DeviceTypeRepository(),
 		private readonly operationSystemRepository = new OperationSystemRepository(),
+		private readonly screenRepository = new ScreenRepository(),
 	) { }
 
-	public getBrowserName(): Maybe<TBrowserName> {
+	public getBrowserName() {
 		return this.browserNameRepository.get()
 	}
 
-	public getDeviceType(): Maybe<TDevices> {
+	public getDeviceType() {
 		return this.deviceTypeRepository.get()
 	}
 
-	public getOperationSystem(): Maybe<TOperationSystemAll> {
+	public getOperationSystem() {
 		return this.operationSystemRepository.get()
+	}
+
+	public getScreenInformation() {
+		return this.screenRepository.get()
 	}
 }
