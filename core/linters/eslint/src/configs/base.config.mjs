6b645f6b1041bcc9274ca93b2897ddd413f693c1
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-06-15 12:45:46 +0200

    chore(linters): eslint - migration base config

---
+++ core/linters/eslint/src/configs/base.config.mjs
@@ -0,0 +1,55 @@
+import eslintConfig from "@eslint/js"
+import globals from "globals"
+
+export default [
+	{
+		...eslintConfig.configs.recommended,
+		ignores: [
+			"build/*",
+			"dist/*",
+			"node_modules/*",
+		],
+		files: [
+			"**/*.js",
+			"**/*.jsx",
+			"**/*.mjs",
+			"**/*.mts",
+			"**/*.ts",
+			"**/*.tsx",
+		],
+		languageOptions: {
+			ecmaVersion: "latest",
+			globals: [
+				...globals.browser,
+				...globals.es2024,
+				...globals.node,
+			],
+			sourceType: "module",
+		},
+		rules: {
+			"eol-last": [
+				"error",
+				"always",
+			],
+			indent: [
+				"error",
+				"tab",
+				{
+					"ignoredNodes": ["PropertyDefinition"]
+				}
+			],
+			"jsx-quotes": [
+				"error",
+				"prefer-double",
+			],
+			semi: [
+				"error",
+				"never",
+			],
+			quotes: [
+				"error",
+				"double",
+			],
+		}
+	},
+]
