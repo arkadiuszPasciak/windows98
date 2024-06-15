8d29a78e2a59f786a2a5eb30a26a281f59af6f6e
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-06-15 12:51:53 +0200

    chore(linters): eslint - add main index config

---
+++ core/linters/eslint/src/configs/index.config.mjs
@@ -0,0 +1,11 @@
+import eslintBaseConfig from "./base.config.mjs"
+import eslintTypescriptConfig from "./typescript.config.mjs"
+import eslintReactConfig from "./react.config.mjs"
+import eslintVueConfig from "./vue.config.mjs"
+
+export default [
+	eslintBaseConfig,
+	eslintTypescriptConfig,
+	eslintReactConfig,
+	eslintVueConfig
+]
