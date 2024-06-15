d73ffe8041b40fd9403628ecae0a8c7d92de6146
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-06-15 12:01:36 +0200

    chore(linters): eslint - migration vue config

---
+++ core/linters/eslint/src/configs/vue.config.mjs
@@ -0,0 +1,15 @@
+// TODO import my own typescript rules
+// TODO check is typescript working in vue files
+
+import eslintVuePlugin from "eslint-plugin-vue"
+
+export default [
+	{
+		...eslintVuePlugin.configs["flat/recommended"],
+		files: ["**/*.vue"],
+		rules: {
+			"vue/require-default-prop": "off",
+			"vue/multi-word-component-names": "off",
+		}
+	},
+]
