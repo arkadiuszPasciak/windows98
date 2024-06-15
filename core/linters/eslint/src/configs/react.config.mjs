b54c1c81330f16e156ca4c6586314d6364bf7b76
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-06-15 12:30:17 +0200

    chore(linters): eslint - migration react config

---
+++ core/linters/eslint/src/configs/react.config.mjs
@@ -0,0 +1,27 @@
+// Todo import my own typescript rules
+import eslintReactPlugin from "eslint-plugin-react"
+import eslintReactHooksPlugin from "eslint-plugin-react-hooks"
+
+export default [
+	{
+		...eslintReactPlugin.configs.recommended,
+		...eslintReactPlugin.configs['jsx-runtime'],
+		...eslintReactHooksPlugin.configs.recommended,
+		files: [
+			"**/*.tsx",
+			"**/*.hook.ts"
+		],
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			}
+		},
+		settings: {
+			react: {
+				version: "18.2.0",
+			},
+		},
+	},
+]
