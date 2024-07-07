d5fe50529ec425fa8ac6e0c9e2bb7efc3dd622cc
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-07-07 13:39:48 +0200

    feat(ma): calculator - export ma-calendar like a web component

--- micro-apps/@bundler/src/wc/index.wc.ts
+++ micro-apps/@bundler/src/wc/index.wc.ts
@@ -1,7 +1,11 @@
 import r2wc from "@r2wc/react-to-web-component"
 import i18nInit from "@windows98/i18n/configs/react.config"
+import { CalculatorView } from "../../../src/ma-calculator/src/ui/views"
 import { CalendarView } from "../../../src/ma-calendar/src/ui/views/calendar.view"
 import { TimezoneView } from "../../../src/ma-timezone/src/ui/views/timezone.view"
+
+const MACalculator = r2wc(CalculatorView)
+customElements.define("ma-calculator", MACalculator)
 
 const MACalendar = r2wc(CalendarView)
 customElements.define("ma-calendar", MACalendar)
