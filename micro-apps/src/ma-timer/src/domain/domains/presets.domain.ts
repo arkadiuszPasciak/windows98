94370f53c1b0e60197d5208c5905fc70b9e7b46c
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-07-14 12:05:19 +0200

    feat(micro-apps): timer - add implementation contract presets

---
+++ micro-apps/src/ma-timer/src/domain/domains/presets.domain.ts
@@ -0,0 +1,47 @@
+import {
+	makeAutoObservable,
+} from "mobx"
+import type { TimerPresetsDomainContract } from "../contracts"
+import {
+	ETimerPresets,
+	ETimerTime,
+} from "../models"
+import { TimerDomainContract } from "../contracts"
+
+export default class TimerPresetsDomain implements TimerPresetsDomainContract {
+	public preset: ETimerPresets = ETimerPresets.THREE_MINUTES
+	private timerDomain: TimerDomainContract
+	private presetTimes: Record<ETimerPresets, number> = {
+		[ETimerPresets.CUSTOM_MINUTES]: 0,
+		[ETimerPresets.THREE_MINUTES]: 3,
+		[ETimerPresets.FIVE_MINUTES]: 5,
+		[ETimerPresets.TEN_MINUTES]: 10,
+		[ETimerPresets.FIFTEEN_MINUTES]: 15,
+	}
+
+	constructor(timerDomain: TimerDomainContract) {
+		this.timerDomain = timerDomain
+		makeAutoObservable(this)
+	}
+
+	public setPreset(preset: ETimerPresets): void {
+		this.setPresetStatus(preset)
+
+		this.resetSecondsAndHours()
+
+		this.setMinutes(preset)
+	}
+
+	private setMinutes(preset: ETimerPresets): void {
+		this.timerDomain.setTime(ETimerTime.MINUTES, this.presetTimes[preset])
+	}
+
+	private setPresetStatus(preset: ETimerPresets): void {
+		if (this.preset !== preset) this.preset = preset
+	}
+
+	private resetSecondsAndHours(): void {
+		if (this.timerDomain.time.seconds !== 0) this.timerDomain.setTime(ETimerTime.SECONDS, 0)
+		if (this.timerDomain.time.hours !== 0) this.timerDomain.setTime(ETimerTime.HOURS, 0)
+	}
+}
