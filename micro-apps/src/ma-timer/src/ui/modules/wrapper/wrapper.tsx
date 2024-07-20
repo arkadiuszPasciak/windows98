fd3305162667f6234f22fdfd00578ccae3345fb8
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-07-20 08:26:42 +0200

    feat(ma): timer - add module wrapper

---
+++ micro-apps/src/ma-timer/src/ui/modules/wrapper/wrapper.tsx
@@ -0,0 +1,11 @@
+import type { FunctionComponent } from "react"
+
+export const Wrapper: FunctionComponent = () => {
+	return (
+		<div
+			data-testid="ma-timer-wrapper"
+		>
+			test wrapper
+		</div>
+	)
+}
--- micro-apps/src/ma-timer/src/ui/views/timer.view.tsx
+++ micro-apps/src/ma-timer/src/ui/views/timer.view.tsx
@@ -1,11 +1,12 @@
 import { TimerDomainContext } from "../context/domain.context"
 import { TimerDomainRoot } from "../../domain/domains"
+import { Wrapper } from "../modules/wrapper"
 
 // TODO: Add the modal to the timer view
 export const TimerView = () => {
 	return (
 		<TimerDomainContext.Provider value={TimerDomainRoot}>
-			<p>123</p>
+			<Wrapper />
 		</TimerDomainContext.Provider>
 	)
 }
