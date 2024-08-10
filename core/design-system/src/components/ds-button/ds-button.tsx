10ca7a7c9177b77c0b721bbee89ccdee5c0cf42c
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-08-10 10:00:24 +0200

    feat(ds): button - add component with hook and types

---
+++ core/design-system/src/components/ds-button/ds-button.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import styles from "./ds-button.module.scss"
+import { DSButtonProps } from "./ds-button.type"
+
+export const DSButton = ({
+	size = "medium",
+	disabled = false,
+	onClick,
+	text,
+	isTextVisible = true,
+	iconLeft,
+	iconRight,
+}: DSButtonProps): JSX.Element => {
+	return (
+		<button
+			aria-label={!isTextVisible ? text : undefined}
+			className={`${styles.DSButton} ${styles[`size-${size}`]}`}
+			type="button"
+			disabled={disabled}
+			onClick={onClick}
+		>
+			{iconLeft}
+			{isTextVisible && text}
+			{iconRight}
+		</button>
+	)
+}
