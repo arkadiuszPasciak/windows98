ff881db4780d7d48069ecdc0057c35a1eb964975
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-08-11 10:35:00 +0200

    style(ma): wrapper

--- micro-apps/src/ma-timer/src/ui/modules/wrapper/wrapper.tsx
+++ micro-apps/src/ma-timer/src/ui/modules/wrapper/wrapper.tsx
@@ -3,10 +3,12 @@ import { Player } from '../player'
 import { Presets } from '../presets'
 import { Screen } from "../screen"
 import type { FunctionComponent } from "react"
+import styles from "./wrapper.module.scss"
 
 export const Wrapper: FunctionComponent = () => {
 	return (
 		<div
+			className={styles.wrapper}
 			data-testid="ma-timer-wrapper"
 		>
 			<Presets />
