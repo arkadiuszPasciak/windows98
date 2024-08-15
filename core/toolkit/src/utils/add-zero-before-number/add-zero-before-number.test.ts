03019e9eb87dda166fa0b826454fe2aacb34ccf0
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-08-15 09:26:29 +0200

    chore(toolkit): install biome, extend config and lint with format all files

--- core/toolkit/src/utils/add-zero-before-number/add-zero-before-number.test.ts
+++ core/toolkit/src/utils/add-zero-before-number/add-zero-before-number.test.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it } from "vitest"
+import { describe, expect, it } from "vitest"
 import { addZeroBeforeNumber } from "./add-zero-before-number.util"
 
 describe("addZeroBeforeNumber", () => {
--- core/toolkit/src/utils/add-zero-before-number/add-zero-before-number.util.ts
+++ core/toolkit/src/utils/add-zero-before-number/add-zero-before-number.util.ts
@@ -1,8 +1,3 @@
-export function addZeroBeforeNumber(
-	number: number,
-	maxNumber: number
-): string {
-	return number < maxNumber
-		? `0${number}`
-		: String(number)
+export function addZeroBeforeNumber(number: number, maxNumber: number): string {
+	return number < maxNumber ? `0${number}` : String(number)
 }
