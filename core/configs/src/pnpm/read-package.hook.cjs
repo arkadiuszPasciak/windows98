0928bb5636f41d495ff1dc012c42b5bf19010ccd
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-08-17 09:07:37 +0200

    feat(configs): pnpm - update read package hook with new dependencies

--- core/configs/src/pnpm/read-package.hook.cjs
+++ core/configs/src/pnpm/read-package.hook.cjs
@@ -1,8 +1,22 @@
 class PNPMReadPackageHook {
 	#dependencies = {
-		libraries: {
+		requirements: {
+			"@biomejs/biome": "1.8.3",
+			"@playwright/experimental-ct-react": "1.45.1",
+			"@vitejs/plugin-react": "4.2.1",
+			i18next: "23.8.2",
+			"lint-staged": "15.2.2",
+			mobx: "6.12.0",
+			"mobx-react-lite": "4.0.4",
 			react: "18.2.0",
+			"react-i18next": "14.0.5",
 			"react-dom": "18.2.0",
+			stylelint: "16.2.1",
+			typescript: "5.4.2",
+			vite: "5.2.8",
+			"vite-plugin-css-injected-by-js": "3.3.1",
+			"vite-plugin-dts": "3.7.0",
+			vitest: "1.3.1",
 		},
 		types: [
 			"dependencies",
@@ -12,26 +26,25 @@ class PNPMReadPackageHook {
 		],
 	}
 
-	run = (packages) => {
-		if (!packages || typeof packages !== "object") {
+	run = (pck) => {
+		if (!pck || typeof pck !== "object") {
 			throw new Error("[PNPMReadPackageHook]: Invalid packages object")
 		}
 
-		this.#checkDependencies(packages)
+		this.#checkDependencies(pck)
 
-		return packages
+		return pck
 	}
 
-	#checkDependencies = (packages) => {
+	#checkDependencies = (pck) => {
 		for (const type of this.#dependencies.types) {
-			for (const dependency of Object.keys(this.#dependencies.libraries)) {
+			for (const dependency of Object.keys(this.#dependencies.requirements)) {
 				if (
-					packages[type]?.[dependency] &&
-					packages[type][dependency] !==
-						this.#dependencies.libraries[dependency]
+					pck[type]?.[dependency] &&
+					pck[type][dependency] !== this.#dependencies.requirements[dependency]
 				) {
 					throw new Error(
-						`[PNPMReadPackageHook]: Dependency version mismatch in "${packages.name}" for ${dependency} of ${type}. Expected: ${this.#dependencies.libraries[dependency]}, Found: ${packages[type][dependency]}`,
+						`[PNPMReadPackageHook]: Dependency version mismatch in "${pck.name}" for ${dependency} of ${type}. Expected: ${this.#dependencies.requirements[dependency]}, Found: ${pck[type][dependency]}`,
 					)
 				}
 			}
