00fbac85db0b530d0c7658c2b169afefdfa2f4b3
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-08-24 07:00:55 +0200

    docs(ci-cd): ci cd config and run command

--- core/ci-cd/src/configs/ci-cd.config.mjs
+++ core/ci-cd/src/configs/ci-cd.config.mjs
@@ -1,75 +1,116 @@
+import { exec } from "node:child_process"
+
+/**
+ * Class representing a CICD command executor.
+ */
 export class CICDCommand {
-	#commands = {
-		build: [
-			"@windows98/app",
-			"@windows98/design-system",
-			"@windows98/micro-apps",
-			"@windows98/micro-services",
-		],
-		"lint:code:run": [
-			"@windows98/app",
-			"@windows98/configs",
-			"@windows98/design-system",
-			"@windows98/docs",
-			"@windows98/i18n",
-			"@windows98/micro-apps",
-			"@windows98/micro-services",
-		],
-		"lint:styles:run": [
-			"@windows98/app",
-			"@windows98/design-system",
-			"@windows98/micro-apps",
-		],
-		"lint:types:run": [
-			"@windows98/app",
-			"@windows98/design-system",
-			"@windows98/docs",
-			"@windows98/i18n",
-			"@windows98/micro-apps",
-			"@windows98/micro-services",
-			"@windows98/toolkit",
-		],
-		"test:ui:run": ["@windows98/design-system", "@windows98/micro-apps"],
-		"test:unit:run": [
-			"@windows98/app",
-			"@windows98/design-system",
-			"@windows98/micro-apps",
-			"@windows98/micro-services",
-			"@windows98/toolkit",
-		],
-	}
+    #commands = {
+        build: [
+            "@windows98/app",
+            "@windows98/design-system",
+            "@windows98/micro-apps",
+            "@windows98/micro-services",
+        ],
+        "lint:code:run": [
+            "@windows98/app",
+            "@windows98/configs",
+            "@windows98/design-system",
+            "@windows98/docs",
+            "@windows98/i18n",
+            "@windows98/micro-apps",
+            "@windows98/micro-services",
+        ],
+        "lint:staged:fix": [
+            "@windows98/app",
+            "@windows98/configs",
+            "@windows98/design-system",
+            "@windows98/docs",
+            "@windows98/i18n",
+            "@windows98/micro-apps",
+            "@windows98/micro-services",
+            "@windows98/toolkit",
+        ],
+        "lint:styles:run": [
+            "@windows98/app",
+            "@windows98/design-system",
+            "@windows98/micro-apps",
+        ],
+        "lint:types:run": [
+            "@windows98/app",
+            "@windows98/design-system",
+            "@windows98/docs",
+            "@windows98/i18n",
+            "@windows98/micro-apps",
+            "@windows98/micro-services",
+            "@windows98/toolkit",
+        ],
+        "test:ui:run": ["@windows98/design-system", "@windows98/micro-apps"],
+        "test:unit:run": [
+            "@windows98/app",
+            "@windows98/design-system",
+            "@windows98/micro-apps",
+            "@windows98/micro-services",
+            "@windows98/toolkit",
+        ],
+    }
 
-	async #runCommand(command) {
-		return new Promise((resolve, reject) => {
-			exec(command, (error, stdout, stderr) => {
-				if (error) {
-					console.error(`[CICDCommand]: error ${stderr}`)
-					reject(error)
-				} else {
-					console.log(stdout)
-					resolve()
-				}
-			})
-		})
-	}
+    /**
+     * Executes a shell command.
+     * @private
+     * @param {string} command - The command to execute.
+     * @returns {Promise<void>} - A promise that resolves when the command completes.
+     */
+    async #runCommand(command) {
+        return new Promise((resolve, reject) => {
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    reject(
+                        new Error(`[CICDCommand]: error executing ${command}: ${stderr}`),
+                    )
+                } else {
+                    console.log(stdout)
+                    resolve()
+                }
+            })
+        })
+    }
 
-	async #runCommands(command) {
-		const workspaces = this.#commands[command]
+    /**
+     * Runs a series of commands for the specified workspace.
+     * @private
+     * @param {string} command - The command to run.
+     * @throws Will throw an error if the command is not provided or not found.
+     * @returns {Promise<void>} - A promise that resolves when all commands complete.
+     */
+    async #runCommands(command) {
+        if (!command) {
+            throw new Error("[CICDCommand]: command is required")
+        }
 
-		try {
-			for (const workspace of workspaces) {
-				console.log(`[CICDCommand/${command}]: ${workspace}`)
-				await this.#runCommand(`pnpm -r --filter ${workspace} ${command}`)
-			}
+        if (!this.#commands[command]) {
+            throw new Error(`[CICDCommand]: command ${command} not found`)
+        }
 
-			console.log(`[CICDCommand/${command}]: all packages successfully!`)
-		} catch (error) {
-			console.error(`[CICDCommand/${command}]: process failed ${error}`)
-			process.exit(1)
-		}
-	}
+        const workspaces = this.#commands[command]
 
-	async run(command) {
-		await this.#runCommands(command)
-	}
+        try {
+            for (const workspace of workspaces) {
+                console.log(`[CICDCommand/${command}]: ${workspace}`)
+                await this.#runCommand(`pnpm -r --filter ${workspace} ${command}`)
+            }
+
+            console.log(`[CICDCommand/${command}]: all packages successfully!`)
+        } catch (error) {
+            throw new Error(`[CICDCommand/${command}]: process failed ${error}`)
+        }
+    }
+
+    /**
+     * Public method to run a specified command.
+     * @param {string} command - The command to run.
+     * @returns {Promise<void>} - A promise that resolves when the command completes.
+     */
+    async run(command) {
+        await this.#runCommands(command)
+    }
 }
