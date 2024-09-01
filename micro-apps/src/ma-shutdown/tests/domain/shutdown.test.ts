9eb0e4b678301e4336eeae125eefe94e8112c4da
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-09-01 10:33:32 +0200

    test(ma): shutdown - update test with method change presets

--- micro-apps/src/ma-shutdown/tests/domain/shutdown.test.ts
+++ micro-apps/src/ma-shutdown/tests/domain/shutdown.test.ts
@@ -1,12 +1,15 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest"
 import { ShutdownDomain } from "../../src/domain/domains"
+import { EShutdownPresets } from "../../src/domain/models"
 
 describe("ShutdownDomain", () => {
 	const shutdownDomain = new ShutdownDomain()
 
 	beforeEach(() => {
-		vi.spyOn(window.location, 'reload').mockImplementation(() => { })
-		vi.spyOn(window.location, 'assign').mockImplementation(() => { })
+		vi.stubGlobal("location", {
+			reload: vi.fn(),
+			href: "",
+		})
 	})
 
 	afterEach(() => {
@@ -20,8 +23,27 @@ describe("ShutdownDomain", () => {
 	})
 
 	it("turnOffApplication should redirect to 'https://google.com'", () => {
-		shutdownDomain.turnOffApplication()
+		shutdownDomain.shutdownApplication()
 
-		expect(window.location.assign).toHaveBeenCalledWith("https://google.com")
+		expect(window.location.href).toBe("https://google.com")
+	})
+
+	it("changePreset should change the preset correctly", () => {
+		shutdownDomain.changePreset(EShutdownPresets.RESTART)
+		expect(shutdownDomain.preset).toBe(EShutdownPresets.RESTART)
+
+		shutdownDomain.changePreset(EShutdownPresets.SHUTDOWN)
+		expect(shutdownDomain.preset).toBe(EShutdownPresets.SHUTDOWN)
+
+		shutdownDomain.changePreset(EShutdownPresets.RESTART)
+		expect(shutdownDomain.preset).toBe(EShutdownPresets.RESTART)
+	})
+
+	it("changePreset should not change the preset if the same preset is passed", () => {
+		shutdownDomain.changePreset(EShutdownPresets.RESTART)
+		expect(shutdownDomain.preset).toBe(EShutdownPresets.RESTART)
+
+		shutdownDomain.changePreset(EShutdownPresets.RESTART)
+		expect(shutdownDomain.preset).toBe(EShutdownPresets.RESTART)
 	})
 })
