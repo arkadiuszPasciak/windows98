1121a229d1b20a3bddb102ef6750170b1b13b043
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-09-01 11:15:21 +0200

    feat(i18n): add locales ma shutdown

--- core/internationalization/i18n/src/locales/micro-apps/pl/index.ts
+++ core/internationalization/i18n/src/locales/micro-apps/pl/index.ts
@@ -1,9 +1,11 @@
 import calendarPL from "./ma-calendar.locale.json"
+import shutdownPL from "./ma-shutdown.locale.json"
 import timerPL from "./ma-timer.locale.json"
 import timezonePL from "./ma-timezone.locale.json"
 
 const localesMicroAppsPL = {
 	...calendarPL,
+	...shutdownPL,
 	...timerPL,
 	...timezonePL,
 }
