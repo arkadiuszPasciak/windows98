d0606b2d7765b617ac903a9d32c203421de2e72d
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-09-01 12:18:08 +0200

    style(ma): shutdown - wrapper and controller in ui

--- micro-apps/src/ma-shutdown/src/ui/modules/wrapper/wrapper.tsx
+++ micro-apps/src/ma-shutdown/src/ui/modules/wrapper/wrapper.tsx
@@ -1,10 +1,11 @@
 import { Controller } from "../controller"
 import { Description } from "../description"
 import { Presets } from "../presets"
+import styles from "./wrapper.module.scss"
 
 export const Wrapper = () => {
 	return (
-		<div>
+		<div className={styles.wrapper}>
 			<Description />
 			<Presets />
 			<Controller />
