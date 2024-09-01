6185b422d6105959d3adc6cf06564c315d6a3e9b
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-09-01 12:46:57 +0200

    build(ma): shutdown

--- micro-apps/@bundler/src/wc/index.wc.ts
+++ micro-apps/@bundler/src/wc/index.wc.ts
@@ -2,6 +2,7 @@ import r2wc from "@r2wc/react-to-web-component"
 import i18nInit from "@windows98/i18n/configs/react.config"
 import { CalculatorView } from "../../../src/ma-calculator/src/ui/views"
 import { CalendarView } from "../../../src/ma-calendar/src/ui/views/calendar.view"
+import { ShutdownView } from "../../../src/ma-shutdown/src/ui/views"
 import { TimerView } from "../../../src/ma-timer/src/ui/views"
 import { TimezoneView } from "../../../src/ma-timezone/src/ui/views"
 
@@ -10,6 +11,9 @@ customElements.define("ma-calculator", MACalculator)
 
 const MACalendar = r2wc(CalendarView)
 customElements.define("ma-calendar", MACalendar)
+
+const MAShutdown = r2wc(ShutdownView)
+customElements.define("ma-shutdown", MAShutdown)
 
 const MATimer = r2wc(TimerView)
 customElements.define("ma-timer", MATimer)
