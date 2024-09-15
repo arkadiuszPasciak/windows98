b3750f1b4ff472e3f7713fe8dee21f8d069c8e30
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-09-15 12:43:07 +0200

    fix(ms): theme - lint test file

--- micro-services/src/ms-theme/tests/domain/theme.test.ts
+++ micro-services/src/ms-theme/tests/domain/theme.test.ts
@@ -1,8 +1,8 @@
-import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { ThemeDomain } from '../../src/domain/domains'
-import { EAttributes, EStorageKeys, EThemes } from '../../src/domain/models'
+import { beforeEach, describe, expect, it, vi } from "vitest"
+import { ThemeDomain } from "../../src/domain/domains"
+import { EAttributes, EStorageKeys, EThemes } from "../../src/domain/models"
 
-describe('ThemeDomain', () => {
+describe("ThemeDomain", () => {
 	let themeDomain: ThemeDomain
 
 	beforeEach(() => {
@@ -26,53 +26,74 @@ describe('ThemeDomain', () => {
 	it("should initialize default theme if theme does not exist in storage", () => {
 		window.localStorage.getItem = vi.fn().mockReturnValue(null)
 		window.localStorage.setItem = vi.fn()
-		const elementMock = { getAttribute: vi.fn().mockReturnValue(null), setAttribute: vi.fn() }
+		const elementMock = {
+			getAttribute: vi.fn().mockReturnValue(null),
+			setAttribute: vi.fn(),
+		}
 		document.querySelector = vi.fn().mockReturnValue(elementMock)
 
 		themeDomain.mountThemeColor()
 
-		expect(window.localStorage.setItem).toHaveBeenCalledWith(EStorageKeys.THEME, EThemes.LIGHT)
-		expect(elementMock.setAttribute).toHaveBeenCalledWith(EAttributes.THEME, EThemes.LIGHT)
+		expect(window.localStorage.setItem).toHaveBeenCalledWith(
+			EStorageKeys.THEME,
+			EThemes.LIGHT,
+		)
+		expect(elementMock.setAttribute).toHaveBeenCalledWith(
+			EAttributes.THEME,
+			EThemes.LIGHT,
+		)
 	})
 
-	it('should return stored theme color', () => {
+	it("should return stored theme color", () => {
 		expect(themeDomain.getThemeColor()).toBe(EThemes.DARK)
 		expect(window.localStorage.getItem).toHaveBeenCalledWith(EStorageKeys.THEME)
 	})
 
-	it('should return default theme color when no theme is stored', () => {
+	it("should return default theme color when no theme is stored", () => {
 		window.localStorage.getItem = vi.fn().mockReturnValue(null)
 		expect(themeDomain.getThemeColor()).toBe(EThemes.LIGHT)
 		expect(window.localStorage.getItem).toHaveBeenCalledWith(EStorageKeys.THEME)
 	})
 
-	it('should set theme color in storage', () => {
+	it("should set theme color in storage", () => {
 		themeDomain.setThemeColor(EThemes.DARK)
-		expect(window.localStorage.setItem).toHaveBeenCalledWith(EStorageKeys.THEME, EThemes.DARK)
+		expect(window.localStorage.setItem).toHaveBeenCalledWith(
+			EStorageKeys.THEME,
+			EThemes.DARK,
+		)
 	})
 
-	it('should not update theme if HTML element is not found', () => {
+	it("should not update theme if HTML element is not found", () => {
 		document.querySelector = vi.fn().mockReturnValue(null)
 		themeDomain.updateTheme(EThemes.DARK)
-		expect(document.querySelector).toHaveBeenCalledWith('html')
+		expect(document.querySelector).toHaveBeenCalledWith("html")
 	})
 
-	it('should not update theme if current theme is the same as new theme', () => {
-		const elementMock = { getAttribute: vi.fn().mockReturnValue(EThemes.DARK), setAttribute: vi.fn() }
+	it("should not update theme if current theme is the same as new theme", () => {
+		const elementMock = {
+			getAttribute: vi.fn().mockReturnValue(EThemes.DARK),
+			setAttribute: vi.fn(),
+		}
 		document.querySelector = vi.fn().mockReturnValue(elementMock)
 		themeDomain.updateTheme(EThemes.DARK)
 		expect(elementMock.setAttribute).not.toHaveBeenCalled()
 	})
 
-	it('should update theme if current theme is different from new theme', () => {
-		const elementMock = { getAttribute: vi.fn().mockReturnValue(EThemes.LIGHT), setAttribute: vi.fn() }
+	it("should update theme if current theme is different from new theme", () => {
+		const elementMock = {
+			getAttribute: vi.fn().mockReturnValue(EThemes.LIGHT),
+			setAttribute: vi.fn(),
+		}
 		document.querySelector = vi.fn().mockReturnValue(elementMock)
 		themeDomain.updateTheme(EThemes.DARK)
-		expect(elementMock.setAttribute).toHaveBeenCalledWith(EAttributes.THEME, EThemes.DARK)
+		expect(elementMock.setAttribute).toHaveBeenCalledWith(
+			EAttributes.THEME,
+			EThemes.DARK,
+		)
 	})
 
-	it('should update theme if theme exists in storage', () => {
-		const updateThemeSpy = vi.spyOn(themeDomain, 'updateTheme')
+	it("should update theme if theme exists in storage", () => {
+		const updateThemeSpy = vi.spyOn(themeDomain, "updateTheme")
 		themeDomain.mountThemeColor()
 		expect(updateThemeSpy).toHaveBeenCalledWith(EThemes.DARK)
 	})
