20f0dc3283e77e28bf93392a3eeb26882fce5b50
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-09-16 09:23:09 +0200

    feat(i18n): ma-theme - add translation

--- core/internationalization/i18n/src/locales/micro-apps/en/index.ts
+++ core/internationalization/i18n/src/locales/micro-apps/en/index.ts
@@ -1,11 +1,13 @@
 import calendarEN from "./ma-calendar.locale.json"
 import shutdownEN from "./ma-shutdown.locale.json"
+import themeEN from "./ma-theme.locale.json"
 import timerEN from "./ma-timer.locale.json"
 import timezoneEN from "./ma-timezone.locale.json"
 
 const localesMicroAppsEN = {
 	...calendarEN,
 	...shutdownEN,
+	...themeEN,
 	...timerEN,
 	...timezoneEN,
 }
--- core/internationalization/i18n/src/locales/micro-apps/pl/index.ts
+++ core/internationalization/i18n/src/locales/micro-apps/pl/index.ts
@@ -1,11 +1,13 @@
 import calendarPL from "./ma-calendar.locale.json"
 import shutdownPL from "./ma-shutdown.locale.json"
+import themePL from "./ma-theme.locale.json"
 import timerPL from "./ma-timer.locale.json"
 import timezonePL from "./ma-timezone.locale.json"
 
 const localesMicroAppsPL = {
 	...calendarPL,
 	...shutdownPL,
+	...themePL,
 	...timerPL,
 	...timezonePL,
 }
