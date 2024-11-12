1f218807bb340e9b5a5ea1482e7774df8dad8fd6
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-11-12 07:26:35 +0100

    feat(app): import ms app config builder and set default config

--- app/src/configs/app/use-app-config.hook.ts
+++ app/src/configs/app/use-app-config.hook.ts
@@ -1,9 +1,14 @@
-import { MSAppConfig } from "@windows98/micro-services"
+import { MSAppConfig, MSAppConfigBuilder } from "@windows98/micro-services"
 import { AppLanguages, AppThemes } from "./app.types"
+
+const defaultConfig = new MSAppConfigBuilder<AppThemes, AppLanguages>()
+	.addLanguage(AppLanguages.ENGLISH)
+	.addTheme(AppThemes.LIGHT)
+	.build()
 
 export function useAppConfig() {
 	const appConfig: MSAppConfig<AppThemes, AppLanguages> =
-		MSAppConfig.getInstance(AppLanguages.ENGLISH, AppThemes.LIGHT)
+		MSAppConfig.getInstance(defaultConfig)
 
 	return {
 		appConfig,
