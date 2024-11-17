59606ebf5ad1a64651437e19086e57578c73c0d1
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-11-17 09:37:21 +0100

    feat(ms): error-handler - add contract, model, domain and export

--- micro-services/src/index.ts
+++ micro-services/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./ms-app-config/src"
 export * from "./ms-app-controller/src"
 export * from "./ms-browser-env/src"
+export * from "./ms-error-handler/src"
 export * from "./ms-storage/src"
 export * from "./ms-theme/src"
---
+++ micro-services/src/ms-error-handler/src/domain/domains/error-handler.domain.ts
@@ -0,0 +1,57 @@
+import type { IError } from "../../models"
+import type { ErrorHandlerDomainContract } from "../contracts"
+
+export class ErrorHandlerDomain implements ErrorHandlerDomainContract {
+	private readonly UNKNOWN_ERROR_MESSAGE = "Unknown error message"
+
+	public CatchError(moduleName: string, methodName: string) {
+		const instance = this
+
+		return (
+			_target: object,
+			_propertyKey: string,
+			descriptor: PropertyDescriptor,
+		): void => {
+			const originalMethod = descriptor.value
+
+			descriptor.value = function (...args: unknown[]) {
+				try {
+					return originalMethod.apply(this, args)
+				} catch (error) {
+					const customError = instance.createError(
+						moduleName,
+						methodName,
+						error,
+					)
+
+					throw customError
+				}
+			}
+		}
+	}
+
+	private createError(
+		moduleName: string,
+		methodName: string,
+		error: Error | unknown,
+	): IError {
+		const errorMessage = this.generateErrorMessage(error)
+
+		const newError = new Error(errorMessage)
+		newError.name = `[MSErrorHandler:${moduleName}] ${methodName}`
+
+		return newError
+	}
+
+	private generateErrorMessage(error: Error | unknown): string {
+		return error instanceof Error && error.message.length
+			? error.message
+			: this.UNKNOWN_ERROR_MESSAGE
+	}
+}
+
+const errorHandlerInstance = new ErrorHandlerDomain()
+
+export const MSErrorHandler = {
+	CatchError: errorHandlerInstance.CatchError.bind(errorHandlerInstance),
+}
