7e2a136538de89bacf3d59bb807fefb08c43e5f5
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-11-20 07:57:30 +0100

    chore(app): ms-app-config - update after changes

--- app/src/configs/app/use-app-config.hook.ts
+++ app/src/configs/app/use-app-config.hook.ts
@@ -1,13 +1,16 @@
 import { MSAppConfig, MSAppConfigBuilder } from "@windows98/micro-services"
-import { AppLanguages, AppThemes } from "./app.types"
+import { type AppConfig, AppLanguages, AppThemes } from "./app.types"
 
-const defaultConfig = new MSAppConfigBuilder<AppThemes, AppLanguages>()
-	.addLanguage(AppLanguages.ENGLISH)
-	.addTheme(AppThemes.LIGHT)
+const defaultConfig = new MSAppConfigBuilder<AppConfig>({
+	theme: undefined,
+	language: undefined,
+})
+	.add("language", AppLanguages.ENGLISH)
+	.add("theme", AppThemes.LIGHT)
 	.build()
 
 export function useAppConfig() {
-	const appConfig: MSAppConfig<AppThemes, AppLanguages> =
+	const appConfig: MSAppConfig<AppConfig> =
 		MSAppConfig.getInstance(defaultConfig)
 
 	return {
