6be1f8c08d94fa1132ecea7aaa6e2e4af5bdabf7
Arkadiusz Paściak <a.pasciak@livespace.io>
2024-12-11 09:26:35 +0100

    feat(app): language - replace service with micro service

--- app/src/configs/app/hooks/init-app-config.hook.ts
+++ app/src/configs/app/hooks/init-app-config.hook.ts
@@ -1,13 +1,18 @@
-import { updateLangAttribute } from "@APP/src/bundles/Settings/LanguageBundle/Services/Language.services"
-import { MSAppConfig, MSBrowserEnv, MSTheme } from "@windows98/micro-services"
+import {
+	MSAppConfig,
+	MSBrowserEnv,
+	MSLanguage,
+	MSTheme,
+} from "@windows98/micro-services"
 import { useI18n } from "vue-i18n"
 import { initConfig } from "../builder"
 import { defaultConfig } from "../configs"
-import type { AppThemes } from "../types"
+import type { AppLanguages, AppThemes } from "../types"
 import { useLocalStorage } from "./use-local-storage.hook"
 
 const msBrowserEnv = new MSBrowserEnv()
 const msTheme = new MSTheme<AppThemes>()
+const msLanguage = new MSLanguage<AppLanguages>()
 
 export function initAppConfig() {
 	const localStorage = useLocalStorage()
@@ -33,7 +38,10 @@ export function initAppConfig() {
 
 	MSAppConfig.getInstance(appConfig)
 
-	// TODO: replace updateLangAttribute with new micro-service
-	if (language) locale.value = language && updateLangAttribute(language)
+	if (language) {
+		locale.value = language
+		msLanguage.updateLanguage(language)
+	}
+
 	if (theme) msTheme.updateTheme(theme)
 }
