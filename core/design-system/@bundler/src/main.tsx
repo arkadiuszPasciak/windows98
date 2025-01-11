f33cf5189690379d24108efd50037cc574ca38be
Arkadiusz Paściak <a.pasciak@livespace.io>
2025-01-11 10:28:38 +0100

    fix(ds): import scss file in main and style dev env

--- core/design-system/@bundler/src/main.tsx
+++ core/design-system/@bundler/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 import { TestComponent } from "./test"
 import "../../src/styles/configs/reset.config.css"
 import "../../src/styles/configs/base.config.css"
-import "../dist/styles/variables.config.css"
+import "../build/styles/variables.config.css"
 import type { Maybe } from "@windows98/toolkit"
 
 const root: Maybe<HTMLElement> = document.getElementById("root")
