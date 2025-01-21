d3a06e2179d665da2e91a408bd3541d6fb510931
Arkadiusz Paściak <a.pasciak@livespace.io>
2025-01-21 06:47:15 +0100

    test(ms): event-bus - blocks

---
+++ micro-services/src/ms-event-bus/tests/event-bus.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from "vitest"
+
+describe.skip("EventBusDomain", () => {
+	it("should register an event listener with on method", async () => {})
+
+	it("should register a one-time event listener with once method", async () => {})
+
+	it("should remove an event listener with off method", async () => {})
+
+	it("should emit an event with emit method", async () => {})
+})
