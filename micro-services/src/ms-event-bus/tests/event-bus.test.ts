e50fd4d6beea98d1156c382d23a2011ad6e8d3bc
Arkadiusz Paściak <a.pasciak@livespace.io>
2025-01-22 06:30:49 +0100

    test(ms): event-bus - implementation

--- micro-services/src/ms-event-bus/tests/event-bus.test.ts
+++ micro-services/src/ms-event-bus/tests/event-bus.test.ts
@@ -1,11 +1,46 @@
-import { describe, it } from "vitest"
+import { describe, expect, it, vi } from "vitest"
+import { MSEventBus } from "../src/domain/domains"
 
-describe.skip("EventBusDomain", () => {
-	it("should register an event listener with on method", async () => {})
+describe("EventBusDomain", () => {
+	it("should register an event listener with on method", async () => {
+		const eventBus = MSEventBus
+		const listener = vi.fn()
 
-	it("should register a one-time event listener with once method", async () => {})
+		eventBus.on("testEvent", listener)
+		eventBus.emit("testEvent")
 
-	it("should remove an event listener with off method", async () => {})
+		expect(listener).toHaveBeenCalled()
+	})
 
-	it("should emit an event with emit method", async () => {})
+	it("should register a one-time event listener with once method", async () => {
+		const eventBus = MSEventBus
+		const listener = vi.fn()
+
+		eventBus.once("testEvent", listener)
+		eventBus.emit("testEvent")
+		eventBus.emit("testEvent")
+
+		expect(listener).toHaveBeenCalledTimes(1)
+	})
+
+	it("should remove an event listener with off method", async () => {
+		const eventBus = MSEventBus
+		const listener = vi.fn()
+
+		eventBus.on("testEvent", listener)
+		eventBus.off("testEvent", listener)
+		eventBus.emit("testEvent")
+
+		expect(listener).not.toHaveBeenCalled()
+	})
+
+	it("should emit an event with emit method", async () => {
+		const eventBus = MSEventBus
+		const listener = vi.fn()
+
+		eventBus.on("testEvent", listener)
+		eventBus.emit("testEvent", { data: "testData" })
+
+		expect(listener).toHaveBeenCalledWith({ data: "testData" })
+	})
 })
