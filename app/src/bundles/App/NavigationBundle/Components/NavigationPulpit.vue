a4c81fc9483e1239af9d8434918482678a31bd62
Arkadiusz Paściak <a.pasciak@livespace.io>
2025-02-14 10:47:54 +0100

    refactor(app): replace AudioBundle with ma-audio-button

--- app/src/bundles/App/NavigationBundle/Components/NavigationPulpit.vue
+++ app/src/bundles/App/NavigationBundle/Components/NavigationPulpit.vue
@@ -1,13 +1,12 @@
 <template>
   <NavigationGrid class="NavigationPulpit">
     <Start />
-    <AudioButton />
+    <ma-audio-button></ma-audio-button>
     <ma-clock></ma-clock>
   </NavigationGrid>
 </template>
 
 <script setup lang="ts">
-import AudioButton from "@APP|Bundles/AudioBundle/Components/AudioButton.vue"
 import NavigationGrid from "@APP|Bundles/NavigationBundle/Components/NavigationGrid.vue"
 import Start from "@APP|Bundles/StartBundle/Components/Start.vue"
 </script>
