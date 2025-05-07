116d75bc9b4ee2c519740b377dd684d8aedaea6e
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-05-07 19:32:50 +0200

    feat(i18n): mf-theme-manager - add locales

--- core/internationalization/i18n/src/locales/micro-frontends/de/index.ts
+++ core/internationalization/i18n/src/locales/micro-frontends/de/index.ts
@@ -1,6 +1,7 @@
 import calendarDE from "./mf-calendar.locale.json"
 import pulpitDE from "./mf-pulpit.locale.json"
 import shutdownDE from "./mf-shutdown.locale.json"
+import themeManagerDE from "./mf-theme-manager.locale.json"
 import timerDE from "./mf-timer.locale.json"
 import timezoneDE from "./mf-timezone.locale.json"
 
@@ -8,6 +9,7 @@ const localesMicroFrontendsDE = {
 	...calendarDE,
 	...pulpitDE,
 	...shutdownDE,
+	...themeManagerDE,
 	...timerDE,
 	...timezoneDE,
 }
