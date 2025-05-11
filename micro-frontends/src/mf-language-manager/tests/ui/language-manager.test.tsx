8d0c6c3e28522a16ac0041be66bc221d6bb5ae76
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-05-11 10:43:24 +0200

    test(mf): language-manager - ui

---
+++ micro-frontends/src/mf-language-manager/tests/ui/language-manager.test.tsx
@@ -0,0 +1,40 @@
+import { expect, test } from "@playwright/experimental-ct-react"
+import { LanguageManagerView } from "../../src/ui/views"
+
+test.describe("LanguageManager", () => {
+	test("As a user, I can view and select languages", async ({ mount }) => {
+		const languages = ["de", "en"]
+		let selectedLanguage = ""
+		const onChangeLanguage = (language: string) => {
+			selectedLanguage = language
+		}
+
+		const component = await mount(
+			<LanguageManagerView
+				languages={languages}
+				onChangeLanguage={onChangeLanguage}
+			/>,
+		)
+		const dropdown = component.getByTestId(
+			"ds-select-select-mf-language-manager",
+		)
+
+		const selectAndVerifyLanguage = async (language: string) => {
+			await dropdown.selectOption(language)
+			const selectedValue = await dropdown.inputValue()
+
+			expect(
+				selectedValue,
+				`The dropdown should reflect the user's selected language value (${language})`,
+			).toBe(language)
+			expect(
+				selectedLanguage,
+				`The onChangeLanguage callback should be called with the selected language value (${language})`,
+			).toBe(language)
+		}
+
+		await selectAndVerifyLanguage("en")
+
+		await selectAndVerifyLanguage("de")
+	})
+})
