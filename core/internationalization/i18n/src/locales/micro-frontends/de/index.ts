a17024aff5226b27e565179e56d2dd7ee5a5f880
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-05-11 15:26:15 +0200

    feat(i18n): language-manager - add locales

--- core/internationalization/i18n/src/locales/micro-frontends/de/index.ts
+++ core/internationalization/i18n/src/locales/micro-frontends/de/index.ts
@@ -1,4 +1,5 @@
 import calendarDE from "./mf-calendar.locale.json"
+import languageManagerDE from "./mf-language-manager.locale.json"
 import pulpitDE from "./mf-pulpit.locale.json"
 import shutdownDE from "./mf-shutdown.locale.json"
 import themeManagerDE from "./mf-theme-manager.locale.json"
@@ -7,6 +8,7 @@ import timezoneDE from "./mf-timezone.locale.json"
 
 const localesMicroFrontendsDE = {
 	...calendarDE,
+	...languageManagerDE,
 	...pulpitDE,
 	...shutdownDE,
 	...themeManagerDE,
