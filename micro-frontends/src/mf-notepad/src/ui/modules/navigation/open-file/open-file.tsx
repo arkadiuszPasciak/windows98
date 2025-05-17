c0aa2449fbe253a53507c177e718a0fcae56274c
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-05-17 12:21:00 +0200

    feat(mf): notepad - add module open-file in ui

---
+++ micro-frontends/src/mf-notepad/src/ui/modules/navigation/open-file/open-file.tsx
@@ -0,0 +1,21 @@
+import { observer } from "mobx-react-lite"
+import type { FunctionComponent } from "react"
+import styles from "../navigation.module.scss"
+import { useOpenFile } from "./use-open-file.hook"
+
+export const OpenFile: FunctionComponent = observer(() => {
+	const { openFile, translations } = useOpenFile()
+
+	return (
+		<>
+			<button
+				className={styles.button}
+				data-testid="mf-notepad-open-file-button"
+				type="button"
+				onClick={openFile}
+			>
+				{translations.button}
+			</button>
+		</>
+	)
+})
