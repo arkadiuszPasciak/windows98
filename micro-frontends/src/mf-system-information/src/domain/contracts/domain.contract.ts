5cf3b2be23cc990957615311e0166a35bb85ff50
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-05-26 20:44:47 +0200

    feat(mf): system-information - add domain

---
+++ micro-frontends/src/mf-system-information/src/domain/contracts/domain.contract.ts
@@ -0,0 +1,7 @@
+import type { Maybe } from "@windows98/toolkit"
+
+export interface SystemInformationDomainContract {
+	browserName: Maybe<string>
+	deviceType: Maybe<string>
+	operatingSystem: Maybe<string>
+}
