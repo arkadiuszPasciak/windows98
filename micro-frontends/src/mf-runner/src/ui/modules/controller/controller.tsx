a004955e5972148fae44fd66760400feb5c555e7
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-05-29 15:44:05 +0200

    feat(mf): runner - controler in ui

---
+++ micro-frontends/src/mf-runner/src/ui/modules/controller/controller.tsx
@@ -0,0 +1,16 @@
+import type { FunctionComponent } from "react"
+import { Cancel } from "./cancel/cancel"
+import styles from "./controller.module.scss"
+import { Ok } from "./ok/ok"
+
+export const Controller: FunctionComponent = () => {
+	return (
+		<div
+			className={styles.controller}
+			data-testid="mf-runner-controller"
+		>
+			<Ok />
+			<Cancel />
+		</div>
+	)
+}
---
+++ micro-frontends/src/mf-runner/src/ui/modules/controller/ok/use-ok.hook.ts
@@ -0,0 +1,25 @@
+import { useTranslation } from "react-i18next"
+import { useConfig, useDomain } from "../../../hooks"
+
+export const useOk = () => {
+	const { t } = useTranslation()
+	const { configContext } = useConfig()
+	const { runnerDomain } = useDomain()
+
+	const translations = {
+		text: t("mf-runner.controller.ok"),
+	}
+
+	const onClick = (): void => {
+		const result = runnerDomain.runProgram(configContext.programs)
+
+		if (result) {
+			configContext.onRunProgram(result)
+		}
+	}
+
+	return {
+		onClick,
+		translations,
+	}
+}
