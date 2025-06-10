fa06e01b17bd996d3cdaacd2e91689bf11017a6a
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-06-10 12:07:20 +0200

    test(mf): tic-tac-toe - board service

---
+++ micro-frontends/src/mf-tic-tac-toe/tests/ui/board.test.ts
@@ -0,0 +1,62 @@
+import { beforeEach, describe, expect, it } from "vitest"
+import { FieldType, PlayerSign } from "../../src/domain/models"
+import { TicTacToeBoardService } from "../../src/domain/services"
+
+describe("TicTacToeBoardService", () => {
+	let ticTacToeBoardService: TicTacToeBoardService
+
+	beforeEach(() => {
+		ticTacToeBoardService = new TicTacToeBoardService()
+	})
+
+	it("generateBoard", () => {
+		const board = ticTacToeBoardService.generateBoard(FieldType.THREE_BY_THREE)
+
+		expect(board, "Should generate board with correct length").toHaveLength(
+			FieldType.THREE_BY_THREE,
+		)
+		expect(
+			board.every((cell) => cell === ""),
+			"All cells should be empty",
+		).toBe(true)
+	})
+
+	describe("chooseCell", () => {
+		it("fill the whole board", () => {
+			const board = ticTacToeBoardService.generateBoard(
+				FieldType.THREE_BY_THREE,
+			)
+			for (let index = 0; index < board.length; index++) {
+				const sign = index % 2 === 0 ? PlayerSign.X : PlayerSign.O
+				ticTacToeBoardService.chooseCell(board, index, sign)
+
+				expect(board[index], `Cell ${index} should be set to ${sign}`).toBe(
+					sign,
+				)
+			}
+		})
+
+		it("invalid index", () => {
+			const board = ticTacToeBoardService.generateBoard(
+				FieldType.THREE_BY_THREE,
+			)
+			ticTacToeBoardService.chooseCell(board, -1, PlayerSign.X)
+			ticTacToeBoardService.chooseCell(board, 9, PlayerSign.O)
+
+			expect(
+				board.every((cell) => cell === ""),
+				"No cells should be set for invalid index",
+			).toBe(true)
+		})
+
+		it("cell already filled", () => {
+			const board = ticTacToeBoardService.generateBoard(
+				FieldType.THREE_BY_THREE,
+			)
+			ticTacToeBoardService.chooseCell(board, 0, PlayerSign.X)
+			ticTacToeBoardService.chooseCell(board, 0, PlayerSign.O)
+
+			expect(board[0], "Cell should not be overwritten").toBe(PlayerSign.X)
+		})
+	})
+})
