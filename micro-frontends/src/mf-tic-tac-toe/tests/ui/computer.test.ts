4aa8510fcf24826ed15eae8f3060a97f734a5f01
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-06-12 15:21:03 +0200

    test(mf): tic-tac-toe - computer service

---
+++ micro-frontends/src/mf-tic-tac-toe/tests/ui/computer.test.ts
@@ -0,0 +1,39 @@
+import { beforeEach, describe, expect, it } from "vitest"
+import { PlayerSign } from "../../src/domain/models"
+import { TicTacToeComputerService } from "../../src/domain/services"
+
+describe("TicTacToeComputerService", () => {
+	let ticTacToeComputerService: TicTacToeComputerService
+
+	beforeEach(() => {
+		ticTacToeComputerService = new TicTacToeComputerService()
+	})
+
+	it("should pick a random cell on an empty board", () => {
+		const boardSize = 3
+		const emptyBoard = Array(boardSize * boardSize).fill("")
+		const move = ticTacToeComputerService.determineNextMove(
+			emptyBoard,
+			boardSize,
+			PlayerSign.X,
+		)
+		expect(move, "Should return a valid index").toBeGreaterThanOrEqual(0)
+		expect(move, "Should return a valid index").toBeLessThan(
+			boardSize * boardSize,
+		)
+	})
+
+	it("should pick an adjacent cell if possible", () => {
+		const boardSize = 3
+
+		const board = ["", "", "", "", PlayerSign.X, "", "", "", ""]
+		const move = ticTacToeComputerService.determineNextMove(
+			board,
+			boardSize,
+			PlayerSign.X,
+		)
+
+		const adjacentIndices = [1, 3, 5, 7]
+		expect(adjacentIndices).toContain(move)
+	})
+})
