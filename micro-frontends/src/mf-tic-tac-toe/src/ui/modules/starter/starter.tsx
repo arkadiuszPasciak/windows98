94e997d935bb32d2685a600d068124167203dc15
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-06-16 10:31:31 +0200

    feat(mf): tic-tac-toe - add module starter in ui

---
+++ micro-frontends/src/mf-tic-tac-toe/src/ui/modules/starter/starter.tsx
@@ -0,0 +1,21 @@
+import type { FunctionComponent } from "react"
+import { ChooseDimension } from "./choose-dimension"
+import { ChooseSign } from "./choose-sign"
+import { EnterYourName } from "./enter-your-name"
+import { StartGame } from "./start-game"
+import styles from "./starter.module.scss"
+import type { StarterProps } from "./starter.type"
+
+export const Starter: FunctionComponent<StarterProps> = ({ onStartGame }) => {
+	return (
+		<div
+			className={styles["starter-wrapper"]}
+			data-testid="mf-tic-tac-toe-starter"
+		>
+			<EnterYourName />
+			<ChooseSign />
+			<ChooseDimension />
+			<StartGame onClick={onStartGame} />
+		</div>
+	)
+}
