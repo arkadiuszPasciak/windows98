f7a7101fb6a9eda2c69d465046be9332ded6e398
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-06-17 14:47:13 +0200

    feat(mf): tic-tac-toe - add module game-board in ui

---
+++ micro-frontends/src/mf-tic-tac-toe/src/ui/modules/game/board/board.tsx
@@ -0,0 +1,32 @@
+import { observer } from "mobx-react-lite"
+import type { FunctionComponent } from "react"
+import { BoardItem } from "./board-item"
+import styles from "./board.module.scss"
+import { useBoard } from "./use-board.hook"
+
+export const Board: FunctionComponent = observer(() => {
+	const { boardCells, boardType, handleCellClick, isGameFinished } = useBoard()
+
+	if (!boardCells) return null
+
+	const disabledClass = isGameFinished ? styles.disabled : undefined
+	const variantClass = styles[`variant-${boardType}`]
+
+	return (
+		<div
+			data-testid="mf-tic-tac-toe-game-board"
+			className={`${styles.board} ${variantClass} ${disabledClass}`}
+		>
+			{boardCells.map((cell, index) => (
+				<BoardItem
+					key={`mf-tic-tac-toe-game-board-item-${
+						// biome-ignore lint/suspicious/noArrayIndexKey: @TODO: fix this problem please
+						index
+					}`}
+					value={cell}
+					onClick={() => handleCellClick(index)}
+				/>
+			))}
+		</div>
+	)
+})
