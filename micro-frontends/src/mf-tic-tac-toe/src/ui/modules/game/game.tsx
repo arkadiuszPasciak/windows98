0744a9f67b96fbc140bc6fe3391a15296978f1f1
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-06-18 16:17:27 +0200

    feat(mf): tic-tac-toe - add module game in ui

---
+++ micro-frontends/src/mf-tic-tac-toe/src/ui/modules/game/game.tsx
@@ -0,0 +1,30 @@
+import type { FunctionComponent } from "react"
+import { Board } from "./board"
+import styles from "./game.module.scss"
+import type { GameProps } from "./game.type"
+import { PlayerSign, PlayerSignType } from "./player-sign"
+import { ResultModal } from "./result-modal/result-modal"
+import { useGame } from "./use-game.hook"
+
+export const Game: FunctionComponent<GameProps> = ({ onRestartGame }) => {
+	const { computerData, humanData } = useGame()
+
+	return (
+		<div
+			className={styles["game-wrapper"]}
+			data-testid="mf-tic-tac-toe-game"
+		>
+			<PlayerSign
+				signType={humanData.sign}
+				playerType={PlayerSignType.HUMAN}
+				playerName={humanData.name}
+			/>
+			<PlayerSign
+				signType={computerData.sign}
+				playerType={PlayerSignType.COMPUTER}
+			/>
+			<Board />
+			<ResultModal onRestartGame={onRestartGame} />
+		</div>
+	)
+}
