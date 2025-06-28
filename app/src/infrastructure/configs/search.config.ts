46112bc6785092d2208144046a93e4392611961f
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-06-28 12:02:26 +0200

    feat(app): infrastructure - add config search programs list

---
+++ app/src/infrastructure/configs/search.config.ts
@@ -0,0 +1,10 @@
+import { Program } from "../types"
+
+export const searchProgramsList = [
+	{ [Program.CALCULATOR]: ["Calculator", "Kalkulator", "Taschenrechner"] },
+	{ [Program.NOTEPAD]: ["Notepad", "Notatnik", "Notizblock"] },
+	{ [Program.SETTINGS]: ["Settings", "Ustawienia", "Einstellungen"] },
+	{ [Program.SHUTDOWN]: ["Shutdown", "Wyłącz", "Abschalten"] },
+	{ [Program.TIC_TAC_TOE]: ["Tic Tac Toe", "Kółko i krzyżyk"] },
+	{ [Program.TIMER]: ["Timer", "Minutnik", "Stoppuhr"] },
+]
