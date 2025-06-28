0d08044feccb86711d5c88f354b86bc96dba5a90
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-06-28 12:01:23 +0200

    feat(app): presentation - import programs portal in main container

--- app/src/presentation/container/main/main.container.tsx
+++ app/src/presentation/container/main/main.container.tsx
@@ -1,9 +1,15 @@
 import type { FunctionComponent } from "react"
+import { ProgramsPortal } from "../../../application/portal"
 import { HomeView } from "../../views"
 import useMainContainer from "./use-main-container.hook"
 
 export const MainContainer: FunctionComponent = () => {
 	useMainContainer()
 
-	return <HomeView />
+	return (
+		<>
+			<HomeView />
+			<ProgramsPortal />
+		</>
+	)
 }
