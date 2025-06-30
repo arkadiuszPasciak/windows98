799c0581afb7df8f5ca91440fb9605ef98d0ca61
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-06-30 18:46:51 +0200

    fix(mf): init i18n in dev mode

--- micro-frontends/@bundler/src/main.tsx
+++ micro-frontends/@bundler/src/main.tsx
@@ -1,19 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
 import "@windows98/design-system/base.config.css"
 import "@windows98/design-system/variables.config.css"
-import i18nInit from "@windows98/i18n/configs/react.config"
-import type { Maybe } from "@windows98/toolkit"
-import { MFTimer } from "../../src/mf-timer/src/ui/views"
+import { bootstrapApp } from "./bootstrap-app"
 
-i18nInit().then(() => {
-	const root: Maybe<HTMLElement> = document.getElementById("root")
-
-	if (!root) return
-
-	ReactDOM.createRoot(root).render(
-		<React.StrictMode>
-			<MFTimer onCloseProgram={() => console.log("timer is closed!")} />
-		</React.StrictMode>,
-	)
-})
+bootstrapApp()
