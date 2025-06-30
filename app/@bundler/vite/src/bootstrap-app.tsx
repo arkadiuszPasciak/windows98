284464990fa023d69bd5fb13c3bc48e48abac1b5
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-06-30 18:39:02 +0200

    fix(i18n): instance react config in vite

--- app/@bundler/vite/src/bootstrap-app.tsx
+++ app/@bundler/vite/src/bootstrap-app.tsx
@@ -1,17 +1,20 @@
-import i18nInit from "@windows98/i18n/configs/react.config"
+import i18n from "@windows98/i18n/configs/react.config"
 import type { Maybe } from "@windows98/toolkit"
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { I18nextProvider } from "react-i18next"
 import { MainContainer } from "../../../src/presentation/containers/main"
 
 export async function bootstrapApp() {
-	await i18nInit()
-
 	const root: Maybe<HTMLElement> = document.getElementById("root")
 
 	if (!root) {
 		throw new Error("Root element not found")
 	}
 
-	ReactDOM.createRoot(root).render(<MainContainer />)
+	ReactDOM.createRoot(root).render(
+		<I18nextProvider i18n={i18n}>
+			<MainContainer />
+		</I18nextProvider>,
+	)
 }
