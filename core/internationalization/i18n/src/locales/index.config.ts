dcecf86ff9c6c1024fc59086032ee18be5444d9e
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-08-22 13:29:15 +0200

    chore(i18n): remove old unused translation keys

--- core/internationalization/i18n/src/locales/index.config.ts
+++ core/internationalization/i18n/src/locales/index.config.ts
@@ -1,40 +1,16 @@
-import localesDE from "../locales/de"
-import localesEN from "../locales/en"
-import localesPL from "../locales/pl"
 import localesMicroFrontendsDE from "./micro-frontends/de"
 import localesMicroFrontendsEN from "./micro-frontends/en"
 import localesMicroFrontendsPL from "./micro-frontends/pl"
 
 const locales = {
 	de: {
-		...localesDE,
 		...localesMicroFrontendsDE,
 	},
 	en: {
-		...localesEN,
 		...localesMicroFrontendsEN,
 	},
 	pl: {
-		...localesPL,
 		...localesMicroFrontendsPL,
-	},
-}
-
-export const localesReact = {
-	de: {
-		translation: {
-			...localesMicroFrontendsDE,
-		},
-	},
-	en: {
-		translation: {
-			...localesMicroFrontendsEN,
-		},
-	},
-	pl: {
-		translation: {
-			...localesMicroFrontendsPL,
-		},
 	},
 }
 
