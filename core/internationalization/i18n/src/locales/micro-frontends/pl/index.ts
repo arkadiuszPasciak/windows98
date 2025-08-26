b47e9fbe27046d5f18bb1270c6f43bbf18e3f34d
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-08-26 17:25:42 +0200

    feat(i18n): mf-case-generator - add translations

--- core/internationalization/i18n/src/locales/micro-frontends/pl/index.ts
+++ core/internationalization/i18n/src/locales/micro-frontends/pl/index.ts
@@ -1,5 +1,6 @@
 import calculatorPL from "./mf-calculator.locale.json"
 import calendarPL from "./mf-calendar.locale.json"
+import caseGeneratorPL from "./mf-case-generator.locale.json"
 import colorGeneratorPL from "./mf-color-generator.locale.json"
 import languageManagerPL from "./mf-language-manager.locale.json"
 import notepadPL from "./mf-notepad.locale.json"
@@ -19,6 +20,8 @@ import webcamPL from "./mf-webcam.locale.json"
 
 const localesMicroFrontendsPL = {
 	...calculatorPL,
+	...calendarPL,
+	...caseGeneratorPL,
 	...calendarPL,
 	...colorGeneratorPL,
 	...languageManagerPL,
