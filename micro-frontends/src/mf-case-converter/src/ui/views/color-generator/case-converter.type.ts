46d214dff76da1f9b1bd2ef85cb4c4b48c6dbfcb
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-08-26 18:09:40 +0200

    fix(mf): case-converter - export

--- micro-frontends/src/mf-case-converter/src/ui/views/color-generator/case-converter.type.ts
+++
@@ -1,3 +0,0 @@
-import type { ConfigProps } from "../../context"
-
-export interface CaseConverterViewProps extends ConfigProps {}
--- micro-frontends/src/mf-case-converter/src/ui/views/color-generator/index.ts
+++
@@ -1,2 +0,0 @@
-export { MFCaseConverter } from "./case-converter.view"
-export type { CaseConverterViewProps } from "./case-converter.type"
