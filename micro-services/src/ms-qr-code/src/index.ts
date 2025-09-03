e7e2507ef62b5dcff660ce31c4adc3dce551d422
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-09-03 10:59:51 +0200

    feat(ms): qr-code - export namespace types

--- micro-services/src/ms-qr-code/src/index.ts
+++ micro-services/src/ms-qr-code/src/index.ts
@@ -1,1 +1,6 @@
 export { MSQRCode } from "./domain/domains"
+
+export namespace MSQRCodeTypes {
+	export type Size = import("./domain/models").QRCodeSize
+	export type Type = import("./domain/models").QRCodeType
+}
