f146085162b9561965fa27909201e319ff04b95b
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-09-08 13:00:38 +0200

    feat(mf): qr-code-generator - add module modal in ui

---
+++ micro-frontends/src/mf-qr-code-generator/src/ui/modules/modal/modal.tsx
@@ -0,0 +1,21 @@
+import { DSModal } from "@windows98/design-system"
+import type { FunctionComponent } from "react"
+import type { ModalProps } from "./modal.type"
+import { useModal } from "./use-modal.hook"
+
+export const Modal: FunctionComponent<ModalProps> = ({ children }) => {
+	const { translations, onCloseProgram } = useModal()
+
+	return (
+		<DSModal
+			id="mf-qr-code-generator"
+			moveWindow={true}
+			onClose={onCloseProgram}
+			title={translations.title}
+			width="400px"
+			height="300px"
+		>
+			{children}
+		</DSModal>
+	)
+}
