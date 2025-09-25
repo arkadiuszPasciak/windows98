f29cf3e5fd84b2b20acf0db361b33369c4836eee
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-09-25 09:59:16 +0200

    test(web-apis): media-devices-api - repository

---
+++ core/web-apis/src/media-devices-api/tests/data/media-devices-api.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, it } from "vitest"
+import {
+	mediaDevicesAPIMock,
+	videoAPIMock,
+} from "../../../../@test/vitest/mocks"
+import { MediaDevicesAPIRepository } from "../../src/data/repositories"
+
+describe("MediaDevicesAPIRepository", () => {
+	const mediaDevicesRepository = new MediaDevicesAPIRepository()
+
+	it("getUserMedia", async () => {
+		const testData = {
+			id: "mock-stream",
+		}
+
+		const streamElementMock = mediaDevicesAPIMock.createMockElement(testData.id)
+		mediaDevicesAPIMock.implementMock(streamElementMock)
+
+		const result = await mediaDevicesRepository.getUserMedia({ video: true })
+		expect(result).toBe(streamElementMock)
+
+		const videoElementMock = videoAPIMock.createMockElement(240, 320)
+		videoElementMock.srcObject = streamElementMock
+
+		expect(
+			videoElementMock.srcObject,
+			"should assign MediaStream to video.srcObject",
+		).toBe(streamElementMock)
+	})
+})
