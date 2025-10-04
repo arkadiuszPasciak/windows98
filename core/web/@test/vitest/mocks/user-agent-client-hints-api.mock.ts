06f2daf9c38b807c8e94f9f6d96127447fc39e76
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-10-04 10:14:39 +0200

    test(web): user-agent-client-hints-api - unit and mock

---
+++ core/web/@test/vitest/mocks/user-agent-client-hints-api.mock.ts
@@ -0,0 +1,24 @@
+interface UserAgentClientHints {
+	brands: { name: string; version: string }[]
+	mobile: boolean
+	platform: string
+}
+
+class UserAgentClientHintsAPIMock {
+	public createMock({ brands, mobile, platform }: UserAgentClientHints) {
+		return {
+			brands,
+			mobile,
+			platform,
+		}
+	}
+
+	public implementMock(userAgentDataMock: UserAgentClientHints) {
+		Object.defineProperty(window.navigator, "userAgentData", {
+			value: userAgentDataMock,
+			configurable: false,
+		})
+	}
+}
+
+export const userAgentClientHintsAPIMock = new UserAgentClientHintsAPIMock()
---
+++ core/web/src/apis/user-agent-client-hints-api/tests/data/user-agent-client-hints-api.test.ts
@@ -0,0 +1,36 @@
+import { beforeAll, describe, expect, it } from "vitest"
+import { userAgentClientHintsAPIMock } from "../../../../../@test/vitest/mocks"
+import { UserAgentClientHintsAPIRepository } from "../../src/data/repositories"
+
+describe("UserAgentClientHintsAPIRepository", () => {
+	const testData = {
+		brands: [
+			{ name: "Google", version: "89" },
+			{ name: "Apple", version: "14" },
+		],
+		mobile: true,
+		platform: "MacOS",
+	}
+
+	const userAgentRepository = new UserAgentClientHintsAPIRepository()
+	const userAgentMock = userAgentClientHintsAPIMock.createMock(testData)
+
+	beforeAll(() => {
+		userAgentClientHintsAPIMock.implementMock(userAgentMock)
+	})
+
+	it("getBrands", async () => {
+		const result = userAgentRepository.getBrands()
+		expect(result).toEqual(testData.brands)
+	})
+
+	it("isMobile", async () => {
+		const result = userAgentRepository.isMobile()
+		expect(result).toBe(testData.mobile)
+	})
+
+	it("getPlatform", async () => {
+		const result = userAgentRepository.getPlatform()
+		expect(result).toBe(testData.platform)
+	})
+})
