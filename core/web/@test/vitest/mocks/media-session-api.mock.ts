e5131b1acc19b7a9bd3f79ea515c6d75eaa6010c
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-10-13 18:35:32 +0200

    test(web): media-session-api - unit and mock

---
+++ core/web/@test/vitest/mocks/media-session-api.mock.ts
@@ -0,0 +1,27 @@
+import type { Maybe } from "@windows98/toolkit"
+import { vi } from "vitest"
+
+class MediaSessionAPIMock {
+	public createMock(
+		metadata: Maybe<MediaMetadata> = null,
+		playbackState: MediaSessionPlaybackState = "none",
+	) {
+		return {
+			metadata,
+			playbackState,
+			setActionHandler: vi.fn(),
+			setPositionState: vi.fn(),
+			addEventListener: vi.fn(),
+			removeEventListener: vi.fn(),
+			dispatchEvent: vi.fn(),
+		} as unknown as MediaSession
+	}
+
+	public implementMock(mediaSessionMock: MediaSession) {
+		vi.stubGlobal("navigator", {
+			mediaSession: mediaSessionMock,
+		})
+	}
+}
+
+export const mediaSessionAPIMock = new MediaSessionAPIMock()
