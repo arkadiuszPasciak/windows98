a37e7a685c084c4bf0367456c83bf899984d9be9
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-10-18 09:49:35 +0200

    fix(web): channel-messaging-api - message port mock

--- core/web/@test/vitest/mocks/channel-messaging-api.mock.ts
+++ core/web/@test/vitest/mocks/channel-messaging-api.mock.ts
@@ -1,10 +1,22 @@
 import { vi } from "vitest"
 
 class ChannelMessagingAPIMock {
+	private MessagePortMock = class {
+		onmessage: ((event: MessageEvent) => void) | null = null
+		onmessageerror: ((event: MessageEvent) => void) | null = null
+		postMessage = vi.fn()
+		start = vi.fn()
+		close = vi.fn()
+		addEventListener = vi.fn()
+		removeEventListener = vi.fn()
+		dispatchEvent = vi.fn()
+	}
+
 	public createMock() {
+		const MessagePortMock = this.MessagePortMock
 		return class {
-			port1 = null
-			port2 = null
+			port1 = new MessagePortMock()
+			port2 = new MessagePortMock()
 		}
 	}
 
