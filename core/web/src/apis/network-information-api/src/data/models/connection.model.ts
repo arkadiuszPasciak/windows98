ff3756e1a62757e60fe67116bd9c03443ac628d1
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-10-21 13:20:19 +0200

    feat(web): network-information-api - models

---
+++ core/web/src/apis/network-information-api/src/data/models/connection.model.ts
@@ -0,0 +1,12 @@
+export type NetworkConnectionType =
+	| "bluetooth"
+	| "cellular"
+	| "ethernet"
+	| "mixed"
+	| "none"
+	| "other"
+	| "unknown"
+	| "wifi"
+	| "wimax"
+
+export type NetworkEffectiveConnectionType = "slow-2g" | "2g" | "3g" | "4g"
