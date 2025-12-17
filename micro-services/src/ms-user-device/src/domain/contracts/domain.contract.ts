c974cdfc2eadd2cf581a49cdb9c821746e645474
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-17 12:43:21 +0100

    feat(ms): user-device - get battery information

--- micro-services/src/ms-user-device/src/domain/contracts/domain.contract.ts
+++ micro-services/src/ms-user-device/src/domain/contracts/domain.contract.ts
@@ -1,6 +1,7 @@
 import type { Maybe } from "@windows98/toolkit"
-import type { BatteryInformation } from "../models"
+import type { BatteryInformation, MemoryInformation } from "../models"
 
 export interface UserDeviceDomainContract {
 	getBatteryInformation(): Promise<Maybe<BatteryInformation>>
+	getMemoryInformation(): Maybe<MemoryInformation>
 }
---
+++ micro-services/src/ms-user-device/src/domain/models/memory.model.ts
@@ -0,0 +1,3 @@
+export interface MemoryInformation {
+	RAMInGB: number
+}
