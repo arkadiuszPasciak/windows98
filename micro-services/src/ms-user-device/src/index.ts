b09c9edc3a9be11cde105436b0049b5c0a9b3cd6
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-19 14:28:35 +0100

    feat(ms): user-device - export types

--- micro-services/src/ms-user-device/src/index.ts
+++ micro-services/src/ms-user-device/src/index.ts
@@ -1,1 +1,8 @@
 export { MSUserDevice } from "./domain/domains"
+
+export namespace MSUserDeviceTypes {
+	export type BatteryInformation = import("./domain/models").BatteryInformation
+	export type MemoryInformation = import("./domain/models").MemoryInformation
+	export type NetworkInformation = import("./domain/models").NetworkInformation
+	export type ScreenInformation = import("./domain/models").ScreenInformation
+}
