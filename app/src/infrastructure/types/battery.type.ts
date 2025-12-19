656964eedaa4d3188a1c3bfc476942455ebe10fa
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-19 14:29:05 +0100

    feat(app): infrastructure - add user device types

---
+++ app/src/infrastructure/types/battery.type.ts
@@ -0,0 +1,3 @@
+import type { MSUserDeviceTypes } from "@windows98/micro-services"
+
+export interface Battery extends MSUserDeviceTypes.BatteryInformation {}
--- app/src/infrastructure/types/config.type.ts
+++ app/src/infrastructure/types/config.type.ts
@@ -1,11 +1,20 @@
+import type { Maybe } from "@windows98/toolkit"
+import type { Battery } from "./battery.type"
 import type { Language } from "./language.type"
+import type { Memory } from "./memory.type"
+import type { Network } from "./network.type"
+import type { Screen } from "./screen.type"
 import type { Theme } from "./theme.type"
 
 // TODO: Add types from browsers, devices, and operating systems
 export interface Config {
+	battery: Maybe<Battery>
 	browserName: string | undefined
 	deviceType: string | undefined
 	language: Language | undefined
+	memory: Maybe<Memory>
+	network: Maybe<Network>
 	operatingSystem: string | undefined
+	screen: Maybe<Screen>
 	theme: Theme | undefined
 }
