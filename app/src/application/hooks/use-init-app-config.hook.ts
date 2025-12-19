c82c5b72c85d5856b25dacfd8cbcf3d0c7cf6ef4
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-19 14:31:24 +0100

    feat(app): infrastructure - init user device data in hook init app config

--- app/src/application/hooks/use-init-app-config.hook.ts
+++ app/src/application/hooks/use-init-app-config.hook.ts
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import { useTranslation } from "react-i18next"
+import { UserDeviceService } from "../../infrastructure/services"
 import { useAppConfig } from "./use-app-config.hook"
 import { useBrowserEnv } from "./use-browser-env.hook"
 import { useLanguageManager } from "./use-language-manager.hook"
@@ -16,36 +17,59 @@ export function useInitAppConfig() {
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: This effect runs only once on mount
 	useEffect(() => {
-		const browserName = BrowserEnvService.getBrowserName()
-		const deviceType = BrowserEnvService.getDeviceType()
-		const operatingSystem = BrowserEnvService.getOperatingSystem()
-		const language = StorageService.get("language")
-		const theme = StorageService.get("theme")
+		const init = async () => {
+			const battery = await UserDeviceService.getBatteryInformation()
+			const browserName = BrowserEnvService.getBrowserName()
+			const deviceType = BrowserEnvService.getDeviceType()
+			const language = StorageService.get("language")
+			const memory = UserDeviceService.getMemoryInformation()
+			const network = UserDeviceService.getNetworkInformation()
+			const operatingSystem = BrowserEnvService.getOperatingSystem()
+			const screen = UserDeviceService.getScreenInformation()
+			const theme = StorageService.get("theme")
 
-		if (browserName) {
-			AppConfigService.set("browserName", browserName)
+			if (battery) {
+				AppConfigService.set("battery", battery)
+			}
+
+			if (browserName) {
+				AppConfigService.set("browserName", browserName)
+			}
+
+			if (deviceType) {
+				AppConfigService.set("deviceType", deviceType)
+			}
+
+			if (language) {
+				i18n.changeLanguage(language)
+				LanguageService.updateLanguage(language)
+				AppConfigService.set("language", language)
+			}
+
+			if (memory) {
+				AppConfigService.set("memory", memory)
+			}
+
+			if (network) {
+				AppConfigService.set("network", network)
+			}
+
+			if (operatingSystem) {
+				AppConfigService.set("operatingSystem", operatingSystem)
+			}
+
+			if (screen) {
+				AppConfigService.set("screen", screen)
+			}
+
+			if (theme) {
+				AppConfigService.set("theme", theme)
+				ThemeService.updateTheme(theme)
+			}
 		}
 
-		if (deviceType) {
-			AppConfigService.set("deviceType", deviceType)
-		}
+		void init()
 
-		if (operatingSystem) {
-			AppConfigService.set("operatingSystem", operatingSystem)
-		}
-
-		if (language) {
-			AppConfigService.set("language", language)
-		}
-
-		if (theme) {
-			AppConfigService.set("theme", theme)
-			ThemeService.updateTheme(theme)
-		}
-
-		if (language) {
-			i18n.changeLanguage(language)
-			LanguageService.updateLanguage(language)
-		}
+		console.log(AppConfigService)
 	}, [])
 }
