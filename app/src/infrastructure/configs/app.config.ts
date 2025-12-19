7d29e777da4e65d626beab546f9b314895f78632
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-19 14:30:19 +0100

    feat(app): infrastructure - extend app config about user device

--- app/src/infrastructure/configs/app.config.ts
+++ app/src/infrastructure/configs/app.config.ts
@@ -2,9 +2,13 @@ import { MSAppConfigBuilder } from "@windows98/micro-services"
 import { type Config, Language, Theme } from "../types"
 
 export const appConfig = new MSAppConfigBuilder<Config>({
+	battery: null,
 	browserName: undefined,
 	deviceType: undefined,
 	language: Language.ENGLISH,
+	memory: null,
+	network: null,
 	operatingSystem: undefined,
+	screen: null,
 	theme: Theme.LIGHT,
 }).build()
