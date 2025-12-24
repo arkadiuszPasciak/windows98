9fb2224d8609b3d30498cf6d8676439d90867be9
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-24 16:01:46 +0100

    test(mf): settings - update mocks

--- micro-frontends/src/mf-settings/tests/ui/settings.test.tsx
+++ micro-frontends/src/mf-settings/tests/ui/settings.test.tsx
@@ -1,21 +1,56 @@
 import { expect, test } from "@playwright/experimental-ct-react"
 import { MFSettings } from "../../src/ui/views"
 
+const testMock = {
+	generalInformation: [
+		{
+			name: "battery",
+			items: [
+				{
+					label: "level",
+					value: "85%",
+				},
+				{
+					label: "status",
+					value: "charging",
+				},
+			],
+		},
+		{
+			name: "browser",
+			items: [
+				{
+					label: "name",
+					value: "chrome",
+				},
+			],
+		},
+	],
+	languages: ["de", "en", "pl"],
+	themes: ["dark", "light"],
+	onChangeLanguage: (language: string) => {
+		console.log(`language ${language} is changed!`)
+	},
+	onChangeTheme: (theme: string) => {
+		console.log(`theme ${theme} is changed!`)
+	},
+	onCloseProgram: () => {
+		console.log("settings is closed!")
+	},
+}
+
 test.describe("Settings", () => {
 	test("As a user, I can switch between different settings tabs", async ({
 		mount,
 	}) => {
 		const component = await mount(
 			<MFSettings
-				onCloseProgram={() => console.log("settings is closed!")}
-				languages={["de", "en", "pl"]}
-				onChangeLanguage={(language: string) => {
-					console.log(`language ${language} is changed!`)
-				}}
-				themes={["dark", "light"]}
-				onChangeTheme={(theme: string) => {
-					console.log(`theme ${theme} is changed!`)
-				}}
+				generalInformation={testMock.generalInformation}
+				onChangeLanguage={testMock.onChangeLanguage}
+				onChangeTheme={testMock.onChangeTheme}
+				onCloseProgram={testMock.onCloseProgram}
+				languages={testMock.languages}
+				themes={testMock.themes}
 			/>,
 		)
 
