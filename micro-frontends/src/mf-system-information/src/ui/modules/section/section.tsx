0c032ddbb8d67f5631879728654e1b0a9dcf1235
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-28 18:58:13 +0100

    fix(mf): system-information - return null and unused css classes

--- micro-frontends/src/mf-system-information/src/ui/modules/section/section.tsx
+++ micro-frontends/src/mf-system-information/src/ui/modules/section/section.tsx
@@ -6,34 +6,32 @@ import { useSection } from "./use-section.hook"
 export const Section = () => {
 	const { sections } = useSection()
 
-	return sections?.map((section) => (
-		<DSFrame
-			key={section.name}
-			id={`mf-system-information-section-${section.name}`}
-			title={section.name}
-			className={styles.section}
-		>
-			<dl
-				className={styles.information}
-				data-testid={`mf-system-information-section-${section.name}`}
+	return (
+		sections?.map((section) => (
+			<DSFrame
+				key={section.name}
+				id={`mf-system-information-section-${section.name}`}
+				title={section.name}
 			>
-				{section.items.map((item) => (
-					<Fragment key={item.label}>
-						<dt
-							className={styles.title}
-							data-testid={`mf-system-information-section-${section.name}-${item.label}-label`}
-						>
-							{item.label}:
-						</dt>
-						<dd
-							className={styles.text}
-							data-testid={`mf-system-information-section-${section.name}-${item.label}-value`}
-						>
-							{item.value}
-						</dd>
-					</Fragment>
-				))}
-			</dl>
-		</DSFrame>
-	))
+				<dl data-testid={`mf-system-information-section-${section.name}`}>
+					{section.items.map((item) => (
+						<Fragment key={item.label}>
+							<dt
+								className={styles.title}
+								data-testid={`mf-system-information-section-${section.name}-${item.label}-label`}
+							>
+								{item.label}:
+							</dt>
+							<dd
+								className={styles.text}
+								data-testid={`mf-system-information-section-${section.name}-${item.label}-value`}
+							>
+								{item.value}
+							</dd>
+						</Fragment>
+					))}
+				</dl>
+			</DSFrame>
+		)) ?? null
+	)
 }
