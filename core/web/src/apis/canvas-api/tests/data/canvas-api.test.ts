75d855ff65d08523c6de9da3a8404f76b4ff565e
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-30 14:55:06 +0100

    test(web): canvas-api - create canvas and convert canvas

--- core/web/src/apis/canvas-api/tests/data/canvas-api.test.ts
+++ core/web/src/apis/canvas-api/tests/data/canvas-api.test.ts
@@ -1,26 +1,59 @@
-import { describe, expect, it } from "vitest"
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest"
 import { canvasAPIMock, videoAPIMock } from "../../../../../@test/vitest/mocks"
 import { CanvasAPIRepository } from "../../src/data/repositories"
+
+const testData = {
+	url: "mock-data-url",
+	height: 240,
+	width: 320,
+	format: "image/png",
+	quality: 0.8,
+}
 
 describe("CanvasAPIRepository", () => {
 	const canvasRepository = new CanvasAPIRepository()
 
-	it("getSnapshot", async () => {
-		const testData = {
-			url: "mock-data-url",
-			height: 240,
-			width: 320,
-		}
-
+	beforeEach(() => {
 		const canvasElementMock = canvasAPIMock.createMock(testData.url)
+		canvasAPIMock.implementMock(canvasElementMock)
+	})
+
+	afterEach(() => {
+		vi.resetAllMocks()
+	})
+
+	it("getSnapshot", async () => {
 		const videoElementMock = videoAPIMock.createMock(
 			testData.height,
 			testData.width,
 		)
 
-		canvasAPIMock.implementMock(canvasElementMock)
-
 		const result = await canvasRepository.getSnapshot(videoElementMock)
 		expect(result).toBe(testData.url)
+	})
+
+	it("createCanvas", () => {
+		const canvas = canvasRepository.createCanvas(
+			testData.width,
+			testData.height,
+		)
+
+		expect(canvas.width).toBe(testData.width)
+		expect(canvas.height).toBe(testData.height)
+	})
+
+	it("convertCanvas", () => {
+		const canvas = canvasRepository.createCanvas(
+			testData.width,
+			testData.height,
+		)
+
+		const dataUrl = canvasRepository.convertCanvas(
+			canvas,
+			testData.format,
+			testData.quality,
+		)
+		expect(typeof dataUrl).toBe("string")
+		expect(dataUrl.startsWith(`mock-data-url`)).toBe(true)
 	})
 })
