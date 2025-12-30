a3d3c1b2f3f4edab7d3ed6549c8fd08c854129bd
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-30 15:57:26 +0100

    test(ms): image-manager - domain

---
+++ micro-services/src/ms-image-manager/tests/domain/image-manager.test.ts
@@ -0,0 +1,38 @@
+import { canvasAPIMock } from "@windows98/web/mocks"
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest"
+import type { ImageFormat, ImageQuality } from "../../src"
+import { ImageManagerDomain } from "../../src/domain/domains"
+
+const testData = {
+	url: "mock-data-url",
+	height: 240,
+	width: 320,
+	format: "image/png" as ImageFormat,
+	quality: 0.8 as ImageQuality,
+}
+
+describe("ImageManagerDomain", () => {
+	let ImageManager: ImageManagerDomain
+
+	beforeEach(() => {
+		ImageManager = new ImageManagerDomain()
+
+		const canvasElementMock = canvasAPIMock.createMock(testData.url)
+		canvasAPIMock.implementMock(canvasElementMock)
+	})
+
+	afterEach(() => {
+		vi.clearAllMocks()
+	})
+
+	it("should convert an image", async () => {
+		const convertedImage = ImageManager.convertImage(
+			testData.format,
+			testData.quality,
+		)
+
+		expect(
+			convertedImage.startsWith(`${testData.format};base64,${testData.url}`),
+		).toBe(true)
+	})
+})
