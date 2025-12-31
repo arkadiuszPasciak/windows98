66a4a78c0b2bca0f975fd42481e500b2815fc240
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-31 13:51:25 +0100

    chore(ms): file-manager - inject url api in save file strategy

--- micro-services/src/ms-file-manager/src/domain/domains/strategies/save-file.strategy.ts
+++ micro-services/src/ms-file-manager/src/domain/domains/strategies/save-file.strategy.ts
@@ -1,7 +1,14 @@
 import { CatchError } from "@windows98/toolkit"
+import { URLAPI } from "@windows98/web"
 import type { SaveFileStrategyContract } from "../../contracts"
 
 export class SaveFileStrategy implements SaveFileStrategyContract {
+	private urlAPI: URLAPI
+
+	constructor() {
+		this.urlAPI = new URLAPI()
+	}
+
 	@CatchError()
 	public async save<FileType extends string>(
 		content: string,
@@ -11,7 +18,7 @@ export class SaveFileStrategy implements SaveFileStrategyContract {
 		try {
 			const blob = new Blob([content], { type })
 			const link = document.createElement("a")
-			link.href = URL.createObjectURL(blob)
+			link.href = this.urlAPI.createObjectURL(blob)
 			link.download = filename
 
 			document.body.appendChild(link)
