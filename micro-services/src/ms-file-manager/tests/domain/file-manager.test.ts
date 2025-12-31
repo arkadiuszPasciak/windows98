1d4bed66942cadf284c8f778c783287ca83c5d14
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2025-12-31 13:47:10 +0100

    test(ms): file-manager - download file strategy

--- micro-services/src/ms-file-manager/tests/domain/file-manager.test.ts
+++ micro-services/src/ms-file-manager/tests/domain/file-manager.test.ts
@@ -1,12 +1,38 @@
+import { urlAPIMock } from "@windows98/web/mocks"
 import { beforeEach, describe, expect, it, vi } from "vitest"
 import { FileManagerDomain } from "../../src/domain/domains/file-manager.domain"
 import { TextFileTypes } from "../../src/domain/models"
+
+const testDataMock = {
+	plainText: "Hello, World!",
+	filename: "hello.txt",
+	type: "text/plain",
+}
 
 describe("FileManagerDomain", () => {
 	let fileManager: FileManagerDomain
 
 	beforeEach(() => {
 		fileManager = new FileManagerDomain()
+	})
+
+	it("should download a file with the given blob and filename", () => {
+		const blob = new Blob([testDataMock.plainText], { type: testDataMock.type })
+		const filename = testDataMock.filename
+
+		const appendChildMock = vi.fn()
+		const removeChildMock = vi.fn()
+
+		urlAPIMock.implementMock(urlAPIMock.createMock("blob:url"))
+
+		vi.spyOn(document.body, "appendChild").mockImplementation(appendChildMock)
+		vi.spyOn(document.body, "removeChild").mockImplementation(removeChildMock)
+
+		fileManager.downloadFile(blob, filename)
+
+		expect(global.URL.createObjectURL).toHaveBeenCalledWith(blob)
+		expect(appendChildMock).toHaveBeenCalled()
+		expect(removeChildMock).toHaveBeenCalled()
 	})
 
 	it("should open a file and return its contents", async () => {
