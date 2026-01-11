e321c2cd398e1a7b543388ef4e255cadce65e452
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2026-01-11 19:47:35 +0100

    fix(ms): image-manager - export models

--- micro-services/src/ms-image-manager/src/index.ts
+++ micro-services/src/ms-image-manager/src/index.ts
@@ -1,1 +1,6 @@
 export { MSImageManager } from "./domain/domains"
+
+export namespace MSImageManagerTypes {
+	export type Format = import("./domain/models").ImageFormat
+	export type Quality = import("./domain/models").ImageQuality
+}
