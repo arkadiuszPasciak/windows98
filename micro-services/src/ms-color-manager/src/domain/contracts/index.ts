c6675505e9ae3e7ec59e8d087cfb6ad3f9b8e1c1
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2026-02-17 11:31:18 +0100

    feat(ms): color-manager - contract converter strategy

--- micro-services/src/ms-color-manager/src/domain/contracts/index.ts
+++ micro-services/src/ms-color-manager/src/domain/contracts/index.ts
@@ -1,4 +1,5 @@
 export * from "./color-converter-strategy.contract"
 export * from "./color-service.contract"
+export * from "./converter-strategy.contract"
 export * from "./domain.contract"
 export * from "./random-color-generator-strategy.contract"
