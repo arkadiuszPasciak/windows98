2630bbe365874f69faff68a8a90a8f5a52291632
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2026-03-02 15:23:53 +0100

    feat(ms): color-manager - add model type map

---
+++ micro-services/src/ms-color-manager/src/domain/models/type-map.model.ts
@@ -0,0 +1,13 @@
+import type { CmykColor } from "./cmyk.model"
+import type { HexColor } from "./hex.model"
+import type { HslColor } from "./hsl.model"
+import type { HsvColor } from "./hsv.model"
+import type { RgbColor } from "./rgb.model"
+
+export type ColorTypeMap = {
+	cmyk: CmykColor
+	hex: HexColor
+	hsl: HslColor
+	hsv: HsvColor
+	rgb: RgbColor
+}
