ae9c97dcc20441eabde1cb0edcab75c24151f01a
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2026-03-24 11:37:48 +0100

    chore(all): update lint-staged 16.2.7 -> 16.4.0

--- core/configs/src/pnpm/read-package.hook.cjs
+++ core/configs/src/pnpm/read-package.hook.cjs
@@ -11,7 +11,7 @@ class PNPMReadPackageHook {
 			"@types/react-dom": "19.2.3",
 			i18next: "25.8.0",
 			lightningcss: "1.32.0",
-			"lint-staged": "16.2.7",
+			"lint-staged": "16.4.0",
 			mobx: "6.15.0",
 			"mobx-react-lite": "4.1.1",
 			react: "19.2.4",
