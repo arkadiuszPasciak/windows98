4dadfca0829f9cfe73651955fb2deb038b7be67a
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2026-04-04 13:05:59 +0200

    chore(all): update vitest 4.1.2

--- core/configs/src/pnpm/read-package.hook.cjs
+++ core/configs/src/pnpm/read-package.hook.cjs
@@ -23,7 +23,7 @@ class PNPMReadPackageHook {
 			vite: "7.3.1",
 			"vite-plugin-css-injected-by-js": "3.5.2",
 			"vite-plugin-dts": "4.5.4",
-			vitest: "4.0.18",
+			vitest: "4.1.2",
 		},
 		types: [
 			"dependencies",
