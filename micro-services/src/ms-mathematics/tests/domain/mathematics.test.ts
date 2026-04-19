a1b441179087bf477044583859cfabf543eac11f
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2026-04-19 14:55:45 +0200

    test(ms): mathematics - remove strategy

--- micro-services/src/ms-mathematics/tests/domain/mathematics.test.ts
+++ micro-services/src/ms-mathematics/tests/domain/mathematics.test.ts
@@ -5,8 +5,18 @@ describe("MathematicsDomain", () => {
 	const msMathematics = MSMathematics
 
 	describe("clear", () => {
-		it(`should clear operation`, () => {
+		it("should clear operation", () => {
 			expect(msMathematics.clear()).toBe("")
+		})
+	})
+
+	describe("remove", () => {
+		it("should remove last character from operation", () => {
+			expect(msMathematics.remove("123")).toBe("12")
+
+			expect(msMathematics.remove("1")).toBe("")
+
+			expect(msMathematics.remove("")).toBe("")
 		})
 	})
 })
