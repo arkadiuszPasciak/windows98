bffe6bb604037584b8b6108bb6389421e5782912
Arkadiusz Paściak <pasciak.arkadiusz@gmail.com>
2026-05-02 14:03:53 +0200

    chore(mf): calculator - update module keypad in ui

--- micro-frontends/src/mf-calculator/src/ui/modules/keypad/use-keypad.hook.ts
+++ micro-frontends/src/mf-calculator/src/ui/modules/keypad/use-keypad.hook.ts
@@ -1,12 +1,33 @@
+import type { Numbers, Sign } from "../../../domain/models"
 import { useCalculator } from "../../hooks"
+import type { OperationButtonProps } from "../operation-button"
 
 export function useKeypad() {
 	const { calculatorDomain } = useCalculator()
 
-	return {
-		addDot: calculatorDomain.addDot,
-		addNumber: calculatorDomain.addNumber,
-		addSymbol: calculatorDomain.addSymbol,
-		summResult: calculatorDomain.summResult,
-	}
+	const addDot = () => calculatorDomain.add(".")
+	const addNumber = (number: Numbers) => calculatorDomain.add(number)
+	const addSign = (sign: Sign) => calculatorDomain.add(sign)
+	const summResult = () => calculatorDomain.equal()
+
+	const buttons: OperationButtonProps[] = [
+		{ id: 7, onClick: () => addNumber(7), variant: "primary" },
+		{ id: 8, onClick: () => addNumber(8), variant: "primary" },
+		{ id: 9, onClick: () => addNumber(9), variant: "primary" },
+		{ id: "/", onClick: () => addSign("/"), variant: "secondary" },
+		{ id: 4, onClick: () => addNumber(4), variant: "primary" },
+		{ id: 5, onClick: () => addNumber(5), variant: "primary" },
+		{ id: 6, onClick: () => addNumber(6), variant: "primary" },
+		{ id: "*", onClick: () => addSign("*"), variant: "secondary" },
+		{ id: 1, onClick: () => addNumber(1), variant: "primary" },
+		{ id: 2, onClick: () => addNumber(2), variant: "primary" },
+		{ id: 3, onClick: () => addNumber(3), variant: "primary" },
+		{ id: "-", onClick: () => addSign("-"), variant: "secondary" },
+		{ id: 0, onClick: () => addNumber(0), variant: "primary" },
+		{ id: ".", onClick: addDot, variant: "primary" },
+		{ id: "=", onClick: summResult, variant: "primary" },
+		{ id: "+", onClick: () => addSign("+"), variant: "secondary" },
+	]
+
+	return { buttons }
 }
